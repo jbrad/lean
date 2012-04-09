7df09f77bc9cef500cd077826f9c607904572a55
Tom McFarlin <tom@tommcfarlin.com>
2012-04-09 09:15:26 -0400

    Changing the 'No Comments' header to a paragraph tag rather than a heading tag.
    Fixes #40

--- comments.php
+++ comments.php
@@ -56,7 +56,7 @@
 
 	<?php if( comments_open() ) { ?>
 		<div id="no-comments" class="clearfix">
-			<h2 class="title"><?php _e( 'No Comments', 'standard' ); ?></h2>
+			<p class="title"><?php _e( 'No Comments', 'standard' ); ?></p>
 			<p><?php _e( 'Be the first to start the conversation!', 'standard' ); ?></p>
 			<?php comment_form(); ?>
 		</div><!-- /#no-comments -->
