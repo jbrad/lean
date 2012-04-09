783535f45da4196d03fb18fe0d1e636010c40b68
Tom McFarlin <tom@tommcfarlin.com>
2012-04-09 09:03:01 -0400

    Implementing support for Yoast Breadcrumbs.
    Fixes #34

--- breadcrumbs.php
+++ breadcrumbs.php
@@ -1,4 +1,13 @@
 <?php 
-if( '' !== get_the_ID() ) {
-	echo Standard_Breadcrumbs::get_breadcrumb_trail( get_the_ID() );
-} // end if 
+if( function_exists( 'yoast_breadcrumb' ) ) {
+
+	yoast_breadcrumb( '', '' );
+
+} else {
+
+	if( '' !== get_the_ID() ) {
+		echo Standard_Breadcrumbs::get_breadcrumb_trail( get_the_ID() );
+	} // end if
+ 
+} // end if
+?>
