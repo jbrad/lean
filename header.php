682ad8c91d8a3313c9bc8fe4c6d86406c7c80db4
Tom McFarlin <tom@tommcfarlin.com>
2012-04-09 09:17:07 -0400

    One more update to the wp_title declaration.
    Fixes #32

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 		<link rel="profile" href="http://gmpg.org/xfn/11" />
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-		<title><?php wp_title(''); ?></title>
+		<title><?php wp_title( '' ); ?></title>
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>
