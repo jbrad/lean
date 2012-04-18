a2b9356e977f2e5a8c872a0acb9266408622c5e4
Tom McFarlin <tom@tommcfarlin.com>
2012-04-18 17:24:48 -0400

    Integrating PressTrends. Will send an email with login information.
    Fixes #96

--- functions.php
+++ functions.php
@@ -17,6 +17,7 @@ include_once( get_template_directory() . '/lib/Standard_Nav_Walker.class.php' );
 	6. Stylesheet and JavaScript Sources
 	7. Custom Filters
 	8. Helper Functions
+	9. PressTrends Integration
 */
 
 /* ----------------------------------------------------------- *
@@ -1535,4 +1536,57 @@ function standard_get_link_post_format_attribute( $attr ) {
 	return $result;
 
 } // end standard_get_link_post_format_attribute
+
+
+/* ----------------------------------------------------------- *
+ * 9. PressTrends Integration
+ * ----------------------------------------------------------- */
+
+// Start of Presstrends Magic
+function presstrends() {
+
+// PressTrends Account API Key
+$api_key = 'ef3qajyvq4zyuyyet9ka49ddhebrioiwaxn9';
+
+// Start of Metrics
+global $wpdb;
+$data = get_transient( 'presstrends_data' );
+if (!$data || $data == ''){
+$api_base = 'http://api.presstrends.io/index.php/api/sites/update/api/';
+$url = $api_base . $api_key . '/';
+$data = array();
+$count_posts = wp_count_posts();
+$count_pages = wp_count_posts('page');
+$comments_count = wp_count_comments();
+$theme_data = get_theme_data(get_stylesheet_directory() . '/style.css');
+$plugin_count = count(get_option('active_plugins'));
+$all_plugins = get_plugins();
+foreach($all_plugins as $plugin_file => $plugin_data) {
+$plugin_name .= $plugin_data['Name'];
+$plugin_name .= '&';}
+$posts_with_comments = $wpdb->get_var("SELECT COUNT(*) FROM {$wpdb->prefix}posts WHERE post_type='post' AND comment_count > 0");
+$comments_to_posts = number_format(($posts_with_comments / $count_posts->publish) * 100, 0, '.', '');
+$pingback_result = $wpdb->get_var('SELECT COUNT(comment_ID) FROM '.$wpdb->comments.' WHERE comment_type = "pingback"');
+$data['url'] = stripslashes(str_replace(array('http://', '/', ':' ), '', site_url()));
+$data['posts'] = $count_posts->publish;
+$data['pages'] = $count_pages->publish;
+$data['comments'] = $comments_count->total_comments;
+$data['approved'] = $comments_count->approved;
+$data['spam'] = $comments_count->spam;
+$data['pingbacks'] = $pingback_result;
+$data['post_conversion'] = $comments_to_posts;
+$data['theme_version'] = $theme_data['Version'];
+$data['theme_name'] = $theme_data['Name'];
+$data['site_name'] = str_replace( ' ', '', get_bloginfo( 'name' ));
+$data['plugins'] = $plugin_count;
+$data['plugin'] = urlencode($plugin_name);
+$data['wpversion'] = get_bloginfo('version');
+foreach ( $data as $k => $v ) {
+$url .= $k . '/' . $v . '/';}
+$response = wp_remote_get( $url );
+set_transient('presstrends_data', $data, 60*60*24);}
+}
+
+// PressTrends WordPress Action
+add_action('admin_init', 'presstrends');
 ?>
