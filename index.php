6211cdd33161287151d2ab875968e807adea7aad
Tom McFarlin <tom@tommcfarlin.com>
2012-05-02 11:39:36 -0400

    Removing whitespace

--- index.php
+++ index.php
@@ -17,9 +17,9 @@
 			<?php if ( 'left_sidebar_layout' == $options['layout'] ) { ?>
 				<?php get_sidebar(); ?>
 			<?php } // end if ?>
-	
-			<div id="main" class="<?php echo 'full_width_layout' == $options['layout'] ? 'span12 fullwidth' : 'span8'; ?> clearfix" role="main">
 
+			<div id="main" class="<?php echo 'full_width_layout' == $options['layout'] ? 'span12 fullwidth' : 'span8'; ?> clearfix" role="main">
+			
 				<?php get_template_part( 'breadcrumbs' ); ?>
 				
 				<?php if ( is_archive() ) { ?>                 
