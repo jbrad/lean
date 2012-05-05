6be7caf2b5700b615d2d479f95c6be9747ddab7d
Tom McFarlin <tom@tommcfarlin.com>
2012-05-05 00:42:01 -0400

    Changing "Insert Into Post" text to something more relevant for uploading site icons
    Related #58

--- js/admin.media-upload.js
+++ js/admin.media-upload.js
@@ -3,7 +3,23 @@
 		
 		// Display the media uploader whtn the 'Upload' button is clicked
 		$('#upload_fav_icon').click(function() {
+			
+			// Show the media uploader
 			tb_show('', 'media-upload.php?type=image&TB_iframe=true');
+			
+			// Change the text of the 'Insert Into Post' button
+			var $submit = null;
+			var submitPoll = setInterval(function() {
+				
+				$submit = $('.savesend input[type="submit"]', $('#TB_iframeContent')[0].contentWindow.document);
+				if($submit.length > 0 && $submit !== null) {
+					/* TODO This will need to be manually localized */
+					$submit.val('Save as Site Icon');
+					clearInterval(submitPoll);
+				} // end if
+				
+			}, 1000);
+			
 		});
 		
 		// Remove the URL of the fav icon
