7a852a6686e82654751d8b912c5e67b5b4eba4e9
Tom McFarlin <tom@tommcfarlin.com>
2012-05-09 23:04:59 -0400

    Additional fixes, improvements, and refinements to the widgets
    Related #116, Related #115, Related #105

--- lib/ad-300x250/views/admin.php
+++ lib/ad-300x250/views/admin.php
@@ -1,10 +1,11 @@
 <div class="standard-ad-300x250-wrapper">
-
-	<?php if( '' == $ad_url ) { ?>
+<!--
 		<img src="<?php echo $ad_src; ?>" alt="" />
-	<?php } else { ?>
 		<a href="<?php echo $ad_url; ?>" target="_blank" class="ad_url"><img src="<?php echo $ad_src; ?>" alt="" /></a>
-	<?php } // end if/else ?>
+-->	
+	<div class="preview_image_container">
+		<img src="<?php echo '' == $ad_src ? '' : $ad_src; ?>" alt="" class="preview_image" />
+	</div><!-- /.preview_image_container -->
 	
 	<!-- Hidden fields used to track uploaded images and links -->
 	<input type="hidden" id="<?php echo $this->get_field_id( 'ad_src' ); ?>" name="<?php echo $this->get_field_name( 'ad_src' ); ?>" value="<?php echo $ad_src; ?>" class="ad_src" />
@@ -13,6 +14,6 @@
 	<!-- /Hidden fields -->
 
 	<a href="javascript:;" class="ad_delete <?php echo ('' == $ad_url && '' == $ad_src) ? 'hidden' : '' ?>"><?php _e( 'Delete Advertisement', 'standard' ); ?></a>
-	<a href="javascript:;" class="ad_upload <?php echo ('' != $ad_url || '' != $ad_src) ? 'hidden' : '' ?>"><?php _e( 'Upload Advertisement', 'standard'); ?></a>
+<!--	<a href="javascript:;" class="ad_upload <?php echo ('' != $ad_url || '' != $ad_src) ? 'hidden' : '' ?>"><?php _e( 'Upload Advertisement', 'standard'); ?></a>-->
 	
 </div><!-- /.standard-ad-300x250-wrapper -->
--- lib/personal-image/views/admin.php
+++ lib/personal-image/views/admin.php
@@ -4,23 +4,23 @@
     
     	<label for="<?php echo $this->get_field_id( 'image_src' ); ?>"><?php _e( 'Personal Image:', 'standard' ); ?></label>
 
-    	<img src="<?php echo $image_src; ?>" alt="" class="preview_image" />
+		<div class="preview_image_container">
+	    	<img src="<?php echo $image_src; ?>" alt="" class="preview_image" />
+	    </div><!-- /.preview_image_container -->
     	
     	<span class="description"><?php _e( 'Maximum width is 300 pixels.', 'standard' ); ?></span>
+    	<a href="javascript:;" class="img_delete <?php echo '' == $image_src ? 'hidden' : '' ?>"><?php _e( 'Delete Image', 'standard' ); ?></a>
     	
 		<!-- Hidden fields used to track uploaded images and links -->
 		<input type="hidden" id="<?php echo $this->get_field_id( 'image_src' ); ?>" name="<?php echo $this->get_field_name( 'image_src' ); ?>" value="<?php echo $image_src; ?>" class="img_src" />
 		<!-- /Hidden -->
-	
-		<a href="javascript:;" class="img_upload <?php echo ('' != $image_src ) ? 'hidden' : '' ?>"><?php _e( 'Upload Image', 'standard'); ?></a>
-		<a href="javascript:;" class="img_delete <?php echo ('' == $image_src ) ? 'hidden' : '' ?>"><?php _e( 'Delete Image', 'standard' ); ?></a>
     	
     </div><!-- /.option -->
     
-    <div class="option">
+    <div class="bio option">
     	<label for="<?php echo $this->get_field_id( 'image_description' ); ?>"><?php _e( 'Bio (Optional):', 'standard' ); ?></label>
-    	<textarea id="<?php echo $this->get_field_id( 'image_description' ); ?>" name="<?php echo $this->get_field_name( 'image_description' ); ?>" rows="3" cols="30"><?php echo $image_description; ?></textarea>
-    	<span class="description"><?php _e( '160 characters limit.', 'standard' ); ?></span>
+    	<textarea id="<?php echo $this->get_field_id( 'image_description' ); ?>" name="<?php echo $this->get_field_name( 'image_description' ); ?>" maxlength="160" rows="3" cols="30"><?php echo $image_description; ?></textarea>
+    	<span class="description"><?php _e( '160 character limit.', 'standard' ); ?></span>
     </div><!-- /.option -->
     
 </div><!-- /.standard-personal-image-wrapper -->
