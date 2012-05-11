1d5fb9f44b00657227bc96b574e94ac201f7a129
Jared Erickson <jared@8bit.io>
2012-05-11 17:51:44 -0400

    updated single page pagination to reflect the structure of the index pagination and not use whatever the heck @tom was doing to call the sizes…

--- pagination.php
+++ pagination.php
@@ -11,19 +11,22 @@
 
 <?php if( is_single() ) { ?>
 
-	<div id="single-post-nav" class="row">
-	
-		<?php $pagination_width = 'full_width_layout' == $options['layout'] || 1 == get_post_meta( get_the_ID(), 'standard_seo_post_level_layout', true ) ? 'span6' : 'span4'; ?>
-		<?php $trunc_limit = 'full_width_layout' == $options['layout'] ? 50 : 30; ?>
-		
-		<?php previous_post_link( '<span class="' . $pagination_width . ' previous-page">%link</span>', _x( '&larr;', 'standard' ) . '&nbsp;' . standard_truncate_text( get_previous_post()->post_title, $trunc_limit ) ); ?>
-		
+	<div id="single-post-nav">
+		<ul class="pager">
+		<?php $trunc_limit = 30; ?>
+			<li class="previous">
+			<?php previous_post_link( '<span class="previous-page">%link</span>', _x( '«', 'standard' ) . '&nbsp;' . standard_truncate_text( get_previous_post()->post_title, $trunc_limit ) ); ?>
+			</li>
 		<?php if( '' == get_previous_post() ) { ?>
-			<?php next_post_link( '<span class="' . ( $options['layout'] == 'full_width_layout' ? 'span12' : 'span8' ) . ' no-previous-page-link next-page">%link</span>', _x( '%title &rarr;', 'standard' ) ); ?>
+			<li class="next">
+			<?php next_post_link( '<span class="' . ( $options['layout'] == 'full_width_layout' ? 'span12' : 'span8' ) . ' no-previous-page-link next-page">%link</span>', _x( '%title »', 'standard' ) ); ?>
+			</li>
 		<?php } else { ?>
-			<?php next_post_link( '<span class="' . $pagination_width . ' next-page">%link</span>', standard_truncate_text( get_next_post()->post_title, $trunc_limit ) . '&nbsp;' . _x( '&rarr;', 'standard' ) ); ?>	
+			<li class="next">
+			<?php next_post_link( '<span class="' . $pagination_width . ' next-page">%link</span>', standard_truncate_text( get_next_post()->post_title, $trunc_limit ) . '&nbsp;' . _x( '»', 'standard' ) ); ?>	
+			</li>
 		<?php } // end if/else ?>
-		
+		</ul>	
 	</div><!-- /#single-post-nav -->
 
 <?php } elseif ( $wp_query->max_num_pages > 1 && ( is_home() || is_archive() || is_search() ) ) { ?>
@@ -33,13 +36,13 @@
 		
 			<?php if( get_next_posts_link() ) { ?>
 				<li class="previous">
-					<?php next_posts_link( __( '<span class="nav-previous meta-nav">&larr; Older</span>', 'standard' ) ); ?>
+					<?php next_posts_link( __( '<span class="nav-previous meta-nav">« Older</span>', 'standard' ) ); ?>
 				</li>			
 			<?php } // end if ?>
 			
 			<?php if( get_previous_posts_link() ) { ?>
 				<li class="next">
-					<?php previous_posts_link( __( '<span class="nav-next meta-nav">Newer &rarr;</span>', 'standard' ) ); ?>
+					<?php previous_posts_link( __( '<span class="nav-next meta-nav">Newer »</span>', 'standard' ) ); ?>
 				</li>
 			<?php } // end if ?>
 		
