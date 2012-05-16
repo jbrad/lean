2c7fe153cd70ef2f6c06caa19abb9be4597f2a15
Tom McFarlin <tom@tommcfarlin.com>
2012-05-16 15:47:00 -0400

    Completing social icon implementation
    Fixes #107

--- social-networking.php
+++ social-networking.php
@@ -1,31 +1,33 @@
-<?php $social_options = get_option( 'standard_theme_social_options' ); ?>
+<?php 
 
-<ul class="nav social-icons clearfix">
-
-	<?php if ( '' != $social_options['twitter'] ) { ?>
-		<li><a class="fademe" href="<?php echo esc_url( $social_options['twitter'] ); ?>" title="<?php esc_attr_e( 'Twitter', 'standard'); ?>" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() . '/images/social/small/twitter.png' ); ?>" alt="<?php esc_attr_e( 'Twitter', 'standard'); ?>" /></a></li>
-	<?php } // end if ?>
-
-	<?php if ( '' != $social_options['facebook'] ) { ?>
-		<li><a class="fademe" href="<?php echo esc_url( $social_options['facebook'] ); ?>" title="<?php esc_attr_e( 'Facebook', 'standard'); ?>" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() . '/images/social/small/facebook.png' ); ?>" alt="<?php esc_attr_e( 'Facebook', 'standard'); ?>" /></a></li>
-	<?php } // end if ?>
-
-	<?php if ( '' != $social_options['google_plus'] ) { ?>
-		<li><a class="fademe" href="<?php echo esc_url( $social_options['google_plus'] ); ?>" title="<?php esc_attr_e( 'Google+', 'standard'); ?>" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() . '/images/social/small/google_plus.png' ); ?>" alt="<?php esc_attr_e( 'Google+', 'standard'); ?>" /></a></li>
-	<?php } // end if ?>
-
-	<?php if ( '' != $social_options['pinterest'] ) { ?>
-		<li><a class="fademe" href="<?php echo esc_url( $social_options['pinterest'] ); ?>" title="<?php esc_attr_e( 'Pinterest', 'standard'); ?>" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() . '/images/social/small/pinterest.png' ); ?>" alt="<?php esc_attr_e( 'Pinterest', 'standard'); ?>" /></a></li>
-	<?php } // end if ?>
+	$social_options = get_option( 'standard_theme_social_options' ); 
+	$social_options = $social_options['active-social-icons'];	
 	
-	<?php if ( '' != $social_options['vimeo'] ) { ?>
-		<li><a class="fademe" href="<?php echo esc_url( $social_options['vimeo'] ); ?>" title="<?php esc_attr_e( 'Vimeo', 'standard'); ?>" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() . '/images/social/small/vimeo.png' ); ?>" alt="<?php esc_attr_e( 'Vimeo', 'standard'); ?>" /></a></li>
-	<?php } // end if ?>
+	$social_icons_urls = explode( ';', $social_options );
+	$html .= '<ul class="nav social-icons clearfix">';
+	foreach( $social_icons_urls as $icon_url ) {
 	
-	<?php if ( '' != $social_options['youtube'] ) { ?>
-		<li><a class="fademe" href="<?php echo esc_url( $social_options['youtube'] ); ?>" title="<?php esc_attr_e( 'YouTube', 'standard'); ?>" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() . '/images/social/small/youtube.png' ); ?>" alt="<?php esc_attr_e( 'YouTube', 'standard'); ?>" /></a></li>
-	<?php } // end if ?>
+		$icon_url_array = explode( '|' , $icon_url );
+		if( count( $icon_url_array ) == 1 ) {
+		
+			$icon = $icon_url_array[0];
+		
+		} else { 
+		
+			$icon = $icon_url_array[0];
+			$url = $icon_url_array[1];
+			
+		} // end if/else
+		
+		$html .= '<li>';
+			$html .= '<a href="' . esc_url( $url ) . '" class="fademe" target="_blank"><img src="' . esc_url( $icon ) . '" alt="" /></a>';
+		$html .= '</li>';
+		
+	} // end foreach
+	$html .= '</ul>';
 	
-	<li><a class="fademe" href="<?php echo esc_url( $social_options['rss'] ); ?>" title="<?php esc_attr_e( 'Subscribe via RSS', 'standard'); ?>" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() . '/images/social/small/rss.png' ); ?>" alt="<?php esc_attr_e( 'Subscribe via RSS', 'standard'); ?>" /></a></li>
+	echo $html;
 	
-</ul>
+?>
+
+
