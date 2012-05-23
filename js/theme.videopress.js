25b0d8bc365cd85048b30ea8aabecbe276731241
Tom McFarlin <tom@tommcfarlin.com>
2012-05-23 12:16:24 -0400

    Updating VideoPress functionality
    Fixes #146

--- js/theme.videopress.js
+++ js/theme.videopress.js
@@ -1,8 +1,20 @@
 (function($) {
 	$(function() {
-		$(window).resize(function() { 
-			$('.video-player').children()
-				.css('width', $('.entry-content').width()); 
-		});
+		sizeVideo();
+		$(window)
+			.load(sizeVideo)
+			.resize(sizeVideo);
 	});
 })(jQuery);
+
+function sizeVideo() {
+
+	// Update the poster size
+	jQuery('.videopress-poster').
+		css('width', jQuery('.entry-content').width());
+		
+	// Update the actual video
+	jQuery('.video-player').children()
+		.css('width', jQuery('.entry-content').width()); 
+
+} // end sizeVideo
