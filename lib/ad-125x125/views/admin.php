7f03d2d57072e06330cdc07514f65dfeb2a92100
Jared Erickson <jared@8bit.io>
2012-05-30 15:19:43 -0400

    styled 125x125 ads
    fixes #116

--- lib/ad-125x125/views/admin.php
+++ lib/ad-125x125/views/admin.php
@@ -4,19 +4,19 @@
 	
 		<div class="row">
 			<span class="left 125x125-1">
-				<img src="<?php echo '' == $ad1_src ? '' : $ad1_src; ?>" alt="" width="80" height="80" class="125x125-1-preview" />
+				<img src="<?php echo '' == $ad1_src ? '' : $ad1_src; ?>" alt="" width="90" height="90" class="125x125-1-preview" />
 				<input type="hidden" id="<?php echo $this->get_field_id( 'ad1_src' ); ?>" name="<?php echo $this->get_field_name( 'ad1_src' ); ?>" class="125x125-1-src" value="<?php echo '' == $ad1_src ? '' : $ad1_src; ?>" />
 				<input type="hidden" id="<?php echo $this->get_field_id( 'ad1_url' ); ?>" name="<?php echo $this->get_field_name( 'ad1_url' ); ?>" class="125x125-1-url" value="<?php echo '' == $ad1_url ? '' : $ad1_url; ?>" />
 				<?php if( '' != $ad1_src ) { ?>
-					<a href="javascript:;" class="125x125-1-delete"><?php _e( 'Delete', 'standard' ); ?></a>
+					<a href="javascript:;" class="125x125-1-delete ad-delete"><?php _e( 'Delete', 'standard' ); ?></a>
 				<?php } // end if ?>
 			</span>
 			<span class="right 125x125-2">
-				<img src="<?php echo '' == $ad2_src ? '' : $ad2_src; ?>" alt="" width="80" height="80" class="125x125-2-preview" />
+				<img src="<?php echo '' == $ad2_src ? '' : $ad2_src; ?>" alt="" width="90" height="90" class="125x125-2-preview" />
 				<input type="hidden" id="<?php echo $this->get_field_id( 'ad2_src' ); ?>" name="<?php echo $this->get_field_name( 'ad2_src' ); ?>" class="125x125-2-src" value="<?php echo '' == $ad2_src ? '' : $ad2_src; ?>" />
 				<input type="hidden" id="<?php echo $this->get_field_id( 'ad2_url' ); ?>" name="<?php echo $this->get_field_name( 'ad2_url' ); ?>" class="125x125-2-url" value="<?php echo '' == $ad2_url ? '' : $ad2_url; ?>" />
 				<?php if( '' != $ad2_src ) { ?>
-					<a href="javascript:;" class="125x125-2-delete"><?php _e( 'Delete', 'standard' ); ?></a>
+					<a href="javascript:;" class="125x125-2-delete ad-delete"><?php _e( 'Delete', 'standard' ); ?></a>
 				<?php } // end if ?>
 			</span>
 		</div><!-- /.row -->
--- lib/ad-125x125/views/widget.php
+++ lib/ad-125x125/views/widget.php
@@ -1,25 +1,30 @@
 <div class="standard-ad-125x125">
 	<div class="ad-row">
-	
-		<span class="left">
-			<?php if( '' != $ad1_url && '' != $ad1_src ) { ?>
-				<a href="<?php echo $ad1_url ?>" target="_blank">
-					<img src="<?php echo $ad1_src ?>" alt="" width="125" height="125" />
-				</a>
-			<?php } elseif( '' != $ad1_src ) { ?>
-				<img src="<?php echo $ad1_src ?>" alt="" width="125" height="125" />
-			<?php } // end if/else ?>
-		</span><!-- /.left -->
-		
-		<span class="right">
-			<?php if( '' != $ad2_url && '' != $ad2_src ) { ?>
-				<a href="<?php echo $ad2_url ?>" target="_blank">
-					<img src="<?php echo $ad2_src ?>" alt="" width="125" height="125" />
-				</a>
-			<?php } elseif( '' != $ad2_src ) { ?>
-				<img src="<?php echo $ad2_src ?>" alt="" width="125" height="125" />
-			<?php } // end if/else ?>
-		</span><!-- /.right -->
+		<ul class="thumbnails">
+			<li class="span2">
+				<?php if( '' != $ad1_url && '' != $ad1_src ) { ?>
+					<a class="thumbnail" href="<?php echo $ad1_url ?>" target="_blank">
+						<img src="<?php echo $ad1_src ?>" alt="" width="125" height="125" />
+					</a>
+				<?php } elseif( '' != $ad1_src ) { ?>
+					<div class="thumbnail">
+						<img src="<?php echo $ad1_src ?>" alt="" width="125" height="125" />
+					</div> <!-- ad thumbnail -->
+				<?php } // end if/else ?>
+			</li><!-- /.left -->
+			
+			<li class="span2">
+				<?php if( '' != $ad2_url && '' != $ad2_src ) { ?>
+					<a class="thumbnail" href="<?php echo $ad2_url ?>" target="_blank">
+						<img src="<?php echo $ad2_src ?>" alt="" width="125" height="125" />
+					</a>
+				<?php } elseif( '' != $ad2_src ) { ?>
+					<div class="thumbnail">
+						<img src="<?php echo $ad2_src ?>" alt="" width="125" height="125" />
+					</div> <!-- ad thumbnail -->
+				<?php } // end if/else ?>
+			</li><!-- /.right -->
+		</ul><!-- /.thumbnails -->
 		
 	</div><!-- /.ad-row -->
 </div><!-- /.standard-ad-125x125-wrapper -->
