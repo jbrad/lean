8ccda545da38065eb9834f9fcb5a2309b81faf3d
Jared Erickson <jared@8bit.io>
2012-05-30 22:00:29 -0400

    updated pagination styles and added icons for them

--- pagination.php
+++ pagination.php
@@ -7,7 +7,6 @@
  */
 ?>
 <?php global $wp_query; ?>
-<?php $options = get_option( 'standard_theme_layout_options' ); ?>
 
 <?php if( is_single() ) { ?>
 
@@ -15,13 +14,12 @@
 		<ul class="pager">
 		<?php $trunc_limit = 30; ?>
 			<li class="previous">
-			<?php previous_post_link( '<span class="previous-page">%link</span>', __( '«', 'standard' ) . '&nbsp;' . standard_truncate_text( get_previous_post()->post_title, $trunc_limit ) ); ?>
+			<?php previous_post_link( '<span class="previous-page">%link</span>', __( '<i class="icon-chevron-left"></i>', 'standard' ) . '&nbsp;' . standard_truncate_text( get_previous_post()->post_title, $trunc_limit ) ); ?>
 			</li>
-		<?php if( '' == get_previous_post() ) { ?>
 			<li class="next">
-			<?php next_post_link( '<span class="' . ( $options['layout'] == 'full_width_layout' ? 'span12' : 'span8' ) . ' no-previous-page-link next-page">%link</span>', __( '%title »', 'standard' ) ); ?>
+			<?php next_post_link( '<span class="no-previous-page-link next-page">%link</span>', __( '%title <i class="icon-chevron-right"></i>', 'standard' ) ); ?>
 			</li>
-		<?php } // end if/else ?>
+
 		</ul>	
 	</div><!-- /#single-post-nav -->
 
@@ -32,13 +30,13 @@
 		
 			<?php if( get_next_posts_link() ) { ?>
 				<li class="previous">
-					<?php next_posts_link( __( '<span class="nav-previous meta-nav">« Older</span>', 'standard' ) ); ?>
+					<?php next_posts_link( __( '<span class="nav-previous meta-nav"><i class="icon-chevron-left"></i> Older</span>', 'standard' ) ); ?>
 				</li>			
 			<?php } // end if ?>
 			
 			<?php if( get_previous_posts_link() ) { ?>
 				<li class="next">
-					<?php previous_posts_link( __( '<span class="nav-next meta-nav">Newer »</span>', 'standard' ) ); ?>
+					<?php previous_posts_link( __( '<span class="nav-next meta-nav">Newer <i class="icon-chevron-right"></i></span>', 'standard' ) ); ?>
 				</li>
 			<?php } // end if ?>
 		
