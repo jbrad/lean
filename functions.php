1ca3231d88599b59ac1abe19f9a72631263a3c3c
Tom McFarlin <tom@tommcfarlin.com>
2012-06-13 17:45:05 -0400

    Removing PressTrends integration.
    Fixes #186

--- functions.php
+++ functions.php
@@ -20,7 +20,6 @@ include_once( get_template_directory() . '/lib/Standard_Nav_Walker.class.php' );
 	6. Stylesheet and JavaScript Sources
 	7. Custom Filters
 	8. Helper Functions
-	9. PressTrends Integration
 */
 
 /* ----------------------------------------------------------- *
@@ -2881,65 +2880,4 @@ function standard_add_plugin( $str_path ) {
 		include_once( get_template_directory() . $str_path );
 	} // end if	
 } // end standard_add_plugin
-
-/* ----------------------------------------------------------- *
- * 9. PressTrends Integration
- * ----------------------------------------------------------- */
-
-// Start of Presstrends Magic
-function presstrends() {
-
-	// PressTrends Account API Key
-	$api_key = '9fh4lc4ki76p5z3evxxhumir728x4f8mfph5';
-	
-	// Start of Metrics
-	global $wpdb;
-	$data = get_transient( 'presstrends_data' );
-	
-	if ( ! $data || $data == '' ){
-	
-		$api_base = 'http://api.presstrends.io/index.php/api/sites/update/api/';
-		$url = $api_base . $api_key . '/';
-		$data = array();
-		$count_posts = wp_count_posts();
-		$count_pages = wp_count_posts( 'page' );
-		$comments_count = wp_count_comments();
-		$theme_data = get_theme_data( get_stylesheet_directory() . '/style.css' );
-		$plugin_count = count( get_option( 'active_plugins' ) );
-		
-		$all_plugins = get_plugins();
-		foreach( $all_plugins as $plugin_file => $plugin_data ) {
-			$plugin_name .= $plugin_data['Name'];
-			$plugin_name .= '&';
-		} // end foreach
-		
-		$posts_with_comments = $wpdb->get_var( "SELECT COUNT(*) FROM {$wpdb->prefix}posts WHERE post_type='post' AND comment_count > 0" );
-		$comments_to_posts = number_format( ( $posts_with_comments / $count_posts->publish ) * 100, 0, '.', '' );
-		$pingback_result = $wpdb->get_var( 'SELECT COUNT(comment_ID) FROM '.$wpdb->comments.' WHERE comment_type = "pingback"' );
-		$data['url'] = stripslashes( str_replace( array( 'http://', '/', ':' ), '', site_url() ) );
-		$data['posts'] = $count_posts->publish;
-		$data['pages'] = $count_pages->publish;
-		$data['comments'] = $comments_count->total_comments;
-		$data['approved'] = $comments_count->approved;
-		$data['spam'] = $comments_count->spam;
-		$data['pingbacks'] = $pingback_result;
-		$data['post_conversion'] = $comments_to_posts;
-		$data['theme_version'] = $theme_data['Version'];
-		$data['theme_name'] = $theme_data['Name'];
-		$data['site_name'] = str_replace( ' ', '', get_bloginfo( 'name' ) );
-		$data['plugins'] = $plugin_count;
-		$data['plugin'] = urlencode($plugin_name);
-		$data['wpversion'] = get_bloginfo('version');
-		
-		foreach ( $data as $k => $v ) {
-			$url .= $k . '/' . $v . '/';
-		} // end foreach
-		
-		$response = wp_remote_get( $url );
-		set_transient('presstrends_data', $data, 60 * 60 * 24 );
-		
-	} // end if
-	
-} // end presstrends
-add_action( 'admin_init', 'presstrends' );
 ?>
