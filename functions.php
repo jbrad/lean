05742d3aa2c0b87e2beb276e1380cfaf5a2a78f2
Tom McFarlin <tom@tommcfarlin.com>
2012-06-14 18:05:00 -0400

    Revert "Additional debugger resolutions for offline_mode"
    This reverts commit 3e605c67d3eab08fcf94e3a3ecd3617ed98f14d2.

--- functions.php
+++ functions.php
@@ -1437,12 +1437,7 @@ function standard_add_maintenance_mode_admin_bar_note() {
 	// Remind the user if they are in maintenance mode
 	$options = get_option( 'standard_theme_global_options' );
 	
-	$offline_mode = '';
-	if( isset( $options['offline_mode'] ) ) {
-		$offline_mode = $options['offline_mode'];
-	}
-	
-	if( 'on' == $offline_mode ) {
+	if( 'on' == $options['offline_mode'] ) {
 		global $wp_admin_bar;
 		$wp_admin_bar->add_node(
 			array(
