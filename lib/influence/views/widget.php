91c4460e78411a55e5d266291982ebe31c28a105
Jared Erickson <jared@lessmade.com>
2012-06-15 13:29:17 -0400

    Styled influence widget
    related #97

--- lib/influence/views/widget.php
+++ lib/influence/views/widget.php
@@ -1,28 +1,34 @@
-<div class="standard-influence-wrapper">
+<div class="widget si-wrapper">
 	
 	<?php if( 'both' == $display || 'total' == $display ) { ?>
-		<div class="standard-influence-total">
-			Total Influence: <?php echo number_format( $this->get_total_influence_count( $twitter, $facebook, $feedburner ) ); ?>
-		</div><!-- /.standard-influence-total -->
+		<div class="si-total">
+			<div class="si-total-number"><?php  echo number_format( $this->get_total_influence_count( $twitter, $facebook, $feedburner ) ); ?></div>
+			<span class="si-total-title">Social Influence</span>
+		</div><!-- /.si-total -->
 	<?php } // end if  ?>
 
 	<?php if( 'both' == $display || 'each' == $display ) { ?>
-		<div class="standard-influence-total">
-			<?php if( '' != $twitter ) { ?>
-				Twitter: <?php echo number_format( $this->twitter_follower_count( $twitter ) ); ?>
-				<br />
-			<?php } // end if ?>
-			
-			<?php if( '' != $facebook ) { ?>
-				Facebook: <?php echo number_format( $this->facebook_like_count( $facebook ) ); ?>
-				<br />
-			<?php } // end if ?>
-			
-			<?php if( '' != $feedburner ) { ?>
-				FeedBurner: <?php echo number_format( $this->feedburner_subscriber_count( $feedburner ) ); ?>
-				<br />
-			<?php } // end if ?>	
-		</div><!-- /.standard-influence-individual -->
+		<div class="si-individual">
+			<ul class="si-indvidual-list block-grid three-up">
+				<?php if( '' != $twitter ) { ?>
+					<li class="si-twitter">
+						<a href="#TODO" title="twitter"><?php echo number_format( $this->twitter_follower_count( $twitter ) ); ?></a>
+					</li>
+				<?php } // end if ?>
+				
+				<?php if( '' != $facebook ) { ?>
+					<li class="si-facebook">
+						<a href="#TODO" title="Facebook"><?php echo number_format( $this->facebook_like_count( $facebook ) ); ?></a>
+					</li>
+				<?php } // end if ?>
+				
+				<?php if( '' != $feedburner ) { ?>
+					<li class="si-rss">
+						<a href="#TODO" title="Feedburner"><?php echo number_format( $this->feedburner_subscriber_count( $feedburner ) ); ?></a>
+					</li>
+				<?php } // end if ?>	
+			</ul>
+		</div><!-- /.si-individual -->
 	<?php } // end if/else ?>
 	
 </div><!-- /.standard-influence-wrapper -->
