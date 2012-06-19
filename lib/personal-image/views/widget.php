73be8d8d7cdd8412d92c0194f7ce699f884de348
Jared Erickson <jared@lessmade.com>
2012-06-19 15:39:14 -0400

    styled personal image widget
    related #191

--- lib/personal-image/views/widget.php
+++ lib/personal-image/views/widget.php
@@ -1,14 +1,14 @@
-<div class="standard-personal-image">
-	
+<div class="widget standard-pi-widget">
+	<div class="standard-pi-pic">
 	<?php if( strlen( trim( $image_url ) ) > 0 ) { ?>
 		<a href="<?php echo $image_url; ?>">
 	<?php } // end if ?>
 	
-		<img class="standard-personal-image" src="<?php echo $image_src; ?>" alt="" />
+		<img src="<?php echo $image_src; ?>" alt="" />
 	
 	<?php if( strlen( trim( $image_url ) ) > 0 ) { ?>
 		</a>
 	<?php } // end if ?>
-	
-	<p class="standard-personal-image_bio"><?php echo $image_description; ?></p>
-</div><!-- /.standard-personal-image -->
+	</div><!-- /.standard-pi-pic -->
+	<p class="standard-pi-bio"><?php echo $image_description; ?></p>
+</div><!-- /.standard-pi-widget -->
