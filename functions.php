3e079a3a5c6bc6d695de07b7d6cce158532cbe9c
Tom McFarlin <tom@tommcfarlin.com>
2012-06-20 15:37:55 -0400

    Updating the Standard title in the browser and moving the Google Analytics hint to placeholder text
    Fixes #226
    Related #215

--- functions.php
+++ functions.php
@@ -47,8 +47,8 @@ add_action( 'after_setup_theme', 'standard_set_theme_localization' );
  */
 function standard_theme_menu() {
 
-	add_menu_page(
-		__( 'standard', 'standard' ),
+	add_menu_page( 
+		__( 'Standard Options', 'standard' ),
 		__( 'Standard', 'standard' ),
 		'administrator',
 		'theme_options',
@@ -754,8 +754,8 @@ function google_analytics_display() {
 			$analytics_id = $option['google_analytics'];
 		} // end if
 		
-		$html = '<input type="text" id="google_analytics" name="standard_theme_global_options[google_analytics]" value="' . esc_attr( $analytics_id ) . '" />';
-		$html .= '&nbsp;<span class="description">' . __( 'Enter the ID only (i.e., UA-000000).', 'standard' ) . '</span>';
+		$html = '<input type="text" id="google_analytics" name="standard_theme_global_options[google_analytics]" placeholder="UA-000000" value="' . esc_attr( $analytics_id ) . '" />';
+		$html .= '&nbsp;<span class="description">' . __( 'Enter the ID only.', 'standard' ) . '</span>';
 		
 		echo $html;
 
