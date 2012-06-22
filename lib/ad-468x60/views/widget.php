22a6fe2a240e0a50a11e359db3a5c6d8b9c74166
Jared Erickson <jared@lessmade.com>
2012-06-22 18:57:23 -0400

    removed width/height from outputted img tag

--- lib/ad-468x60/views/widget.php
+++ lib/ad-468x60/views/widget.php
@@ -1,7 +1,7 @@
 <?php echo $args['before_widget']; ?>
 	<?php if( '' == $ad_url ) { ?>
-		<img src="<?php echo $ad_src; ?> " alt="" width="468" height="60" />
+		<img src="<?php echo $ad_src; ?> " alt="" />
 	<?php } else { ?>
-		<a href="<?php echo $ad_url; ?>" target="_blank"><img src="<?php echo $ad_src; ?> " alt="" width="468" height="60" /></a>
+		<a href="<?php echo $ad_url; ?>" target="_blank"><img src="<?php echo $ad_src; ?> " alt="" /></a>
 	<?php } // end if/else ?>
 <?php echo $args['after_widget']; ?>
