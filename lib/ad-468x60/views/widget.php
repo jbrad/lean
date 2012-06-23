435cb9b9b940dfb733f35a79a7b64671f0c7118d
Jared Erickson <jared@lessmade.com>
2012-06-23 10:33:08 -0500

    updated 468 widget to display default widget when no image is uploaded (not if no ur) fixing @tommcfarlin bad coding.

--- lib/ad-468x60/views/widget.php
+++ lib/ad-468x60/views/widget.php
@@ -1,6 +1,6 @@
 <?php echo $args['before_widget']; ?>
-	<?php if( '' == $ad_url ) { ?>
-		<a class="thumbnail" href="http://standardtheme.com" target="_blank">
+	<?php if( '' == $ad_src ) { ?>
+		<a href="http://standardtheme.com" target="_blank">
 			<img src="<?php echo get_template_directory_uri() . '/lib/ad-468x60/images/standard-468.jpg' ?>" alt="Standard" />
 		</a>
 	<?php } else { ?>
