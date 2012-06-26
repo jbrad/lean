9401394e87eb254974e745df0bc4b1aca803af23
Tom McFarlin <tom@tommcfarlin.com>
2012-06-26 15:30:30 -0400

    Revert "Fixing sidebar issues displaying errors in WordPress 3.3."
    This reverts commit d14736407d938e73281c720e84d0a56a7b1b87a8.

--- sidebar.php
+++ sidebar.php
@@ -14,7 +14,7 @@
 		<?php
 		$activity = new Activity_Tabs();
 		$activity->widget(
-			array(),
+			null,
 			array(
 				'post_count'	=> 	10,
 				'popular_count'	=>	10,
@@ -24,8 +24,9 @@
 		
 		$ads_125 = new Standard_Ad_125x125();
 		$ads_125->widget(
-			array(),
-			array()
+			null,
+			array(
+			)
 		);
 		?>
 		
