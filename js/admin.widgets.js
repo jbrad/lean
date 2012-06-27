b58c6ff73d5746c7ae5ca969c9f56fd7ad6c63c3
Tom McFarlin <tom@tommcfarlin.com>
2012-06-27 09:56:05 -0400

    Making sure the widget header has an id before trying to apply styles to it.

--- js/admin.widgets.js
+++ js/admin.widgets.js
@@ -1,7 +1,10 @@
 (function($) {
 	$(function() {
 		$('h4').each(function() {
-			if($(this).parent().parent().parent().attr('id').indexOf('standard-') > 0) {
+
+			var $parent = $(this).parent().parent().parent();
+
+			if($parent.attr('id') !== undefined && $parent.attr('id') !== false && $parent.attr('id').indexOf('standard-') > 0) {
 				$(this).parents('.widget-top').addClass('standard-widget-top');
 			} // end if
 		});
