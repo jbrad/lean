809f8bf52535614aa1fcc03b8d21a546474380c7
Jared Erickson <jared@lessmade.com>
2012-06-28 15:59:19 -0400

    added a class if there is a logo, to allow text to stay absolute over a header image

--- header.php
+++ header.php
@@ -88,10 +88,21 @@
 			$header_image = get_header_image();
 			$head_class = ! empty( $header_image ) ? 'imageyup' : 'imageless';
 		?>
+		
+		<?php
+		
+			// TODO: tom clean up
+			// check to see if there is a logo
+			 if( '' == $presentation_options['logo'] ) {
+				 $hgroup_class = 'no-logo'; 
+				 } else {
+				 $hgroup_class = 'has-logo';
+				 }
+		?>
 			<div id="header" class="<?php echo $head_class ?>">
 				<div id="head-wrapper" class="container clearfix">
 
-						<div id="hgroup" class="clearfix">			
+						<div id="hgroup" class="clearfix <?php echo $hgroup_class ?>">			
 								<div id="logo">
 									<?php if( is_single() || is_page() ) { ?>
 										
