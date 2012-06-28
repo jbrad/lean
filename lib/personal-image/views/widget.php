506672713ab4293dd782a1a0ab0bcec4abb7de2f
Tom McFarlin <tom@tommcfarlin.com>
2012-06-27 22:02:32 -0400

    Hiding the personal image widget when no image has been specified.  Also updating the JavaScript to properly reflect character counts on refresh / save of the page.
    Fixes #259

--- lib/personal-image/views/widget.php
+++ lib/personal-image/views/widget.php
@@ -1,18 +1,23 @@
-<?php echo $args['before_widget']; ?>
-	<div class="standard-pi-pic">
-		<?php if( 0 < strlen( trim( $image_url ) ) ) { ?>
-			<a href="<?php echo $image_url; ?>">
+<?php if( '' != $image_src ) { ?>	
+	<?php echo $args['before_widget']; ?>
+		<div class="standard-pi-pic">
+		
+			<?php if( 0 < strlen( trim( $image_url ) ) ) { ?>
+				<a href="<?php echo $image_url; ?>">
+			<?php } // end if ?>
+		
+			
+			<img src="<?php echo $image_src; ?>" alt="" />
+	
+			
+			<?php if( 0 < strlen( trim( $image_url ) ) ) { ?>
+				</a>
+			<?php } // end if ?>
+		</div><!-- /.standard-pi-pic -->
+		
+		<?php if( '' != trim( $image_description ) ) { ?>
+			<p class="standard-pi-bio"><?php echo $image_description; ?></p>
 		<?php } // end if ?>
 		
-			<img src="<?php echo '' == $image_src ? get_template_directory_uri() . '/lib/personal-image/css/fake-personal.jpg' : $image_src; ?>" alt="" />
-		
-		<?php if( 0 < strlen( trim( $image_url ) ) ) { ?>
-			</a>
-		<?php } // end if ?>
-	</div><!-- /.standard-pi-pic -->
-	
-	<?php if( '' != trim( $image_description ) ) { ?>
-		<p class="standard-pi-bio"><?php echo $image_description; ?></p>
-	<?php } // end if ?>
-	
-<?php echo $args['after_widget']; ?>
+	<?php echo $args['after_widget']; ?>
+<?php } // end if ?>
