0467fa5472aa4a927d3a52d4734d4d3c6c5d0a3c
Tom McFarlin <tom@tommcfarlin.com>
2012-06-29 11:18:20 -0400

    Resolving an issue with video press in full width
    Fixes #280

--- js/theme.videopress.js
+++ js/theme.videopress.js
@@ -17,4 +17,18 @@ function sizeVideo() {
 	jQuery('.video-player').children()
 		.css('width', jQuery('.video-player').parent().width()); 
 
+	// If the site is full-width, fix the float issue ont he thumbnail
+	if( jQuery('.full-width').length > 0 ) {
+	
+		jQuery('.videopress-poster')
+			.css('float', 'left')
+			.height('100%');
+			
+		jQuery('.videopress-placeholder')
+			.children('div:last')
+			.children('img')
+			.css('margin-top', '-40px');
+			
+	} // end if
+
 } // end sizeVideo
