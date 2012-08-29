8e8ba290dbe30733a6a24306e021949c7c23ca55
Tom McFarlin <tom@tommcfarlin.com>
2012-08-29 13:05:13 -0400

    Adding Google Custom Search v2.
    Related #318

--- lib/google-custom-search/views/admin.php
+++ lib/google-custom-search/views/admin.php
@@ -1,9 +1,9 @@
 <div class="google-custom-search wrapper">
 
     <div class="option">
-    	<p><?php _e( 'Paste your Google Custom Search Engine code here. ', 'standard' ); ?>
+    	<p><?php _e( 'Paste your Google Custom Search Engine ID here. ', 'standard' ); ?>
     	<?php _e( '<em><a href="http://docs.8bit.io/standard/widgets/google-custom-search-widget/" target="_blank">Learn More</a>.</em>', 'standard'); ?></p>
-    	<textarea id="<?php echo $this->get_field_id( 'gcse_content' ); ?>" name="<?php echo $this->get_field_name( 'gcse_content' ); ?>" rows="10" cols="30"><?php echo $gcse_content; ?></textarea>
+    	<input id="<?php echo $this->get_field_id( 'gcse_content' ); ?>" name="<?php echo $this->get_field_name( 'gcse_content' ); ?>" rows="10" cols="30" value="<?php echo $gcse_content; ?>" />
     </div><!-- /.option -->
     
 </div><!-- /wrapper -->
--- lib/google-custom-search/views/widget.php
+++ lib/google-custom-search/views/widget.php
@@ -1,5 +1,14 @@
 <?php if( '' != $gcse_content ) { ?>
 	<?php echo $args['before_widget']; ?>
-		<?php echo $gcse_content; ?>
+	<script type="text/javascript">
+	  (function() {
+	    var cx = '<?php echo $gcse_content; ?>';
+	    var gcse = document.createElement('script'); gcse.type = 'text/javascript'; gcse.async = true;
+	    gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+	        '//www.google.com/cse/cse.js?cx=' + cx;
+	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(gcse, s);
+	  })();
+	</script>
+	<gcse:searchbox-only resultsUrl="<?php echo get_permalink( get_page_by_path( 'search-results' ) ); ?>" newWindow="false" queryParameterName="q"></gcse:searchbox-only>
 	<?php echo $args['after_widget']; ?>
 <?php } // end if ?>
