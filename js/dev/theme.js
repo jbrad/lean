807597fb8f0747b4aa40b03a759f6025b9f9c720
Tom McFarlin <tom@tommcfarlin.com>
2012-11-19 09:02:36 -0500

    Fixing positioning of ad widget with the logo
    Fixes #385

--- js/dev/theme.js
+++ js/dev/theme.js
@@ -20,6 +20,30 @@ function moveSidebarInLeftSidebarLayout($) {
 	"use strict";
 	$(function() {
 		
+		var iHeaderHeight, iWidgetHeight, iMargin;
+		
+		// Properly position the header widget, but only do so after the window is loaded
+		if( 1 === $('.header-widget').length && 1 === $('#logo').length ) {
+		
+			$(window).load(function() {
+								
+				// Get the header and the widget heights
+				iHeaderHeight = parseInt($('#hgroup').height(), 10);
+				iWidgetHeight = parseInt($('.header-widget').height(), 10);
+				
+				// Calculate the margins based on the size of the header and the widget
+				if(iHeaderHeight > iWidgetHeight) {
+					iMargin = ( iHeaderHeight / 2 ) - ( iWidgetHeight / 2 );
+				} else {
+					iMargin = ( iWidgetHeight / 2 ) - ( iHeaderHeight / 2 );
+				} // end if/else
+				
+				$('.header-widget').css('margin-top', iMargin);
+					
+			});
+			
+		} // end if
+		
 		// Bootstrap Multi-Level Menus
 		$('.submenu').hover(function() {
 			
