2d78471aba3b19a2679aa9be4ca3acecb581408e
Tom McFarlin <tom@tommcfarlin.com>
2012-11-19 14:16:06 -0500

    Removing the "Gloriously Wide" standard styles
    Related #410

--- functions.php
+++ functions.php
@@ -2694,11 +2694,11 @@ function standard_add_theme_stylesheets() {
 	wp_deregister_style('grunion.css');
 
 	// bootstrap
-	wp_register_style( 'bootstrap', get_template_directory_uri() . '/css/lib/bootstrap.min.css' );
+	wp_register_style( 'bootstrap', get_template_directory_uri() . '/css/lib/bootstrap.css' );
 	wp_enqueue_style( 'bootstrap' ); 
 
 	// bootstrap-responsive
-	wp_register_style( 'bootstrap-responsive', get_template_directory_uri() . '/css/lib/bootstrap-responsive.min.css' );
+	wp_register_style( 'bootstrap-responsive', get_template_directory_uri() . '/css/lib/bootstrap-responsive.css' );
 	wp_enqueue_style( 'bootstrap-responsive' ); 
 
 	// if VideoPress is loaded, we need to add styles for responsive
