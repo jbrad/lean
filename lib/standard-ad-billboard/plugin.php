c08190dc8137814ae73262f5ba675dfc8135ce0d
Tom McFarlin <tom@tommcfarlin.com>
2013-02-10 09:58:33 -0500

    Displaying the placeholder for the billboard
    Fixes #448

--- lib/standard-ad-billboard/plugin.php
+++ lib/standard-ad-billboard/plugin.php
@@ -29,9 +29,9 @@ class Standard_Ad_468x60 extends WP_Widget {
 		global $pagenow;
 		if( 'themes.php' != $pagenow ) {
 		
-			add_action( 'admin_print_styles', array( &$this, 'register_admin_styles' ) );
-			add_action( 'admin_enqueue_scripts', array( &$this, 'register_admin_scripts' ) );
-			add_action( 'wp_enqueue_scripts', array( &$this, 'register_widget_styles' ) );
+			add_action( 'admin_print_styles', array( $this, 'register_admin_styles' ) );
+			add_action( 'admin_enqueue_scripts', array( $this, 'register_admin_scripts' ) );
+			add_action( 'wp_enqueue_scripts', array( $this, 'register_widget_styles' ) );
 			
 		} // end if
 		
@@ -117,7 +117,7 @@ class Standard_Ad_468x60 extends WP_Widget {
 	 * @version	1.0
 	 */
 	public function register_admin_styles() {
-		wp_enqueue_style( 'standard-ad-468x60', get_template_directory_uri() . '/lib/standard-ad-billboard/css/admin.css', array( 'thickbox' ), false, STANDARD_THEME_VERSION );
+		wp_enqueue_style( 'standard-ad-468x60', get_template_directory_uri() . '/lib/standard-ad-billboard/css/admin.css', STANDARD_THEME_VERSION );
 	} // end register_admin_styles
 
 	/** 
