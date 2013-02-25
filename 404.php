3915a4756c7b929a79fd35a921d71cac425f2ee7
Tom McFarlin <tom@tommcfarlin.com>
2013-02-25 11:05:03 -0500

    Removing a space after the ellipse.
    Related #472

--- 404.php
+++ 404.php
@@ -18,7 +18,7 @@
 					<div id="nothing-found" class="no-results not-found">
 						<div class="entry-content clearfix">
 							<img src="<?php echo esc_url( get_template_directory_uri() . '/images/lightning.png' ); ?>" alt="<?php esc_attr_e( '404', 'standard'); ?>" />
-							<h1 class="404-title"><span><?php _e( '404', 'standard' ); ?></span> <?php _e( 'Whoa... you broke the Internet!', 'standard' ); ?></h1>
+							<h1 class="404-title"><span><?php _e( '404', 'standard' ); ?></span> <?php _e( 'Whoa...you broke the Internet!', 'standard' ); ?></h1>
 							<p><?php _e( 'The specified address does not contain a page or blog post at this time', 'standard' ); ?>. <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php esc_attr_e( 'Page Not Found', 'standard' ); ?>"><?php _e( 'Click here to return home.', 'standard' ); ?></a></p>
 							<?php standard_get_search_form(); ?>
 							
