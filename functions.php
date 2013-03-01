07e87cfbe764ca8eb9467ee385354de9d5a33cea
Tom McFarlin <tom@tommcfarlin.com>
2013-03-01 17:26:19 -0500

    updating version numbers

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Standard 3.2
+ * Standard 3.2.3
  * Standard is a sleek, exacting product designed for uncluttered and sophisticated presentation of your content on desktop and mobile devices.
  *
  * This file enables core features of Standard including sidebars, menus, post thumbnails, post formats, header, backgrounds, and more.
@@ -32,7 +32,7 @@
  */
  
 // Define a Standard version. This is used for cache-busting stylesheets, JavaScript, and for serializing the version in the database
-define( 'STANDARD_THEME_VERSION', '3.2.2' );
+define( 'STANDARD_THEME_VERSION', '3.2.3' );
  
 // Imports the Standard_Nav_Walker for the custom menu functionality.
 include_once( get_template_directory() . '/lib/Standard_Nav_Walker.class.php' );
