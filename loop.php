7957bf2072951033e2d7791e85af524620e01c84
Tom McFarlin <tom@tommcfarlin.com>
2013-05-03 14:29:53 -0400

    removing some whitespace and the comment describing the loop since it's redundant with the name of the file

--- loop.php
+++ loop.php
@@ -4,7 +4,7 @@
  * 
  * @package Standard
  * @since 	3.0
- * @version	3.0
+ * @version	3.1
  */
 ?>
 
@@ -47,7 +47,7 @@
 			</div><!-- /.post-header-meta -->
 		</div><!-- /.title-wrap -->
 
-	</div> <!-- /.post-header -->
+	</div><!-- /.post-header -->
 
 	<div id="content-<?php the_ID(); ?>" class="entry-content clearfix">
 		<?php if( ( is_category() || is_archive() || is_home() ) && has_excerpt() ) { ?>
@@ -68,27 +68,26 @@
 	
 	<div class="post-meta clearfix">
 
-			<div class="meta-date-cat-tags pull-left">
+		<div class="meta-date-cat-tags pull-left">
+		
+			<?php $category_list = get_the_category_list( __( ', ', 'standard' ) ); ?>
+			<?php if( $category_list ) { ?>
+				<?php printf( '<span class="the-category">' . __( 'In %1$s', 'standard' ) . '</span>', $category_list ); ?>
+			<?php } // end if ?>
 			
-				<?php $category_list = get_the_category_list( __( ', ', 'standard' ) ); ?>
-				<?php if( $category_list ) { ?>
-					<?php printf( '<span class="the-category">' . __( 'In %1$s', 'standard' ) . '</span>', $category_list ); ?>
-				<?php } // end if ?>
-				
-				<?php $tag_list = get_the_tag_list( '', __( ', ', 'standard' ) ); ?>
-				<?php if( $tag_list ) { ?>
-					<?php printf( '<span class="the-tags">' . __( '%1$s', 'standard' ) . '</span>', $tag_list ); ?>
-				<?php } // end if ?>
-				
-			</div><!-- /meta-date-cat-tags -->
+			<?php $tag_list = get_the_tag_list( '', __( ', ', 'standard' ) ); ?>
+			<?php if( $tag_list ) { ?>
+				<?php printf( '<span class="the-tags">' . __( '%1$s', 'standard' ) . '</span>', $tag_list ); ?>
+			<?php } // end if ?>
 			
-			<div class="meta-comment-link pull-right">
-				<a class="pull-right post-link" href="<?php the_permalink(); ?>" title="<?php esc_attr_e( 'permalink', 'standard' ); ?>">&nbsp;<img src="<?php echo esc_url( get_template_directory_uri() . '/images/icn-permalink.png' ); ?>" alt="<?php esc_attr_e( 'permalink', 'standard' ); ?>" /></a>
-				<?php if ( '' != get_post_format() ) { ?>
-					<span class="the-comment-link"><?php comments_popup_link( __( 'Leave a comment', 'standard' ), __( '1 Comment', 'standard' ), __( '% Comments', 'standard' ), '', ''); ?></span>
-				<?php } // end if ?>
-			</div><!-- /meta-comment-link -->
+		</div><!-- /meta-date-cat-tags -->
+		
+		<div class="meta-comment-link pull-right">
+			<a class="pull-right post-link" href="<?php the_permalink(); ?>" title="<?php esc_attr_e( 'permalink', 'standard' ); ?>">&nbsp;<img src="<?php echo esc_url( get_template_directory_uri() . '/images/icn-permalink.png' ); ?>" alt="<?php esc_attr_e( 'permalink', 'standard' ); ?>" /></a>
+			<?php if ( '' != get_post_format() ) { ?>
+				<span class="the-comment-link"><?php comments_popup_link( __( 'Leave a comment', 'standard' ), __( '1 Comment', 'standard' ), __( '% Comments', 'standard' ), '', ''); ?></span>
+			<?php } // end if ?>
+		</div><!-- /meta-comment-link -->
 
 	</div><!-- /.post-meta -->
-
 </div><!-- /#post -->
