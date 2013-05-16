7218d7033fefd9ec997f531679166febda9fbda1
Tom McFarlin <tom@tommcfarlin.com>
2013-05-16 10:20:56 -0400

    fixing the request mechanism for creating the results page
    fixes #478

--- lib/google-custom-search/plugin.php
+++ lib/google-custom-search/plugin.php
@@ -13,38 +13,38 @@ class Google_Custom_Search extends WP_Widget {
 	/*--------------------------------------------------------*
 	 * Constructor
 	 *--------------------------------------------------------*/
-	
+
 	/**
 	 * Initializes the widget's classname, description, and JavaScripts.
-	 */ 
+	 */
 	function __construct() {
 
 		$widget_opts = array(
-			'classname' 	=> __( 'standard-google-custom-search', 'standard' ), 
+			'classname' 	=> __( 'standard-google-custom-search', 'standard' ),
 			'description' 	=> __( 'Easily add Google Custom Search to your Standard-powered blog.', 'standard' )
-		);	
+		);
 		$this->WP_Widget( 'standard-google-custom-search', __( 'Google Custom Search', 'standard' ), $widget_opts );
-		
+
 		add_action( 'admin_print_styles', array( $this, 'load_admin_stylesheets') );
-		
+
 		// If this widget isn't active and our search results page exists, let's delete it
 		if( is_active_widget( false, false, $this->id_base, true ) ) {
-		
+
 			$this->create_search_results_template();
-			
+
 		// If the widget is now active, then create the search results template; otherwise, delete what exists
 		} else {
-		
+
 			$this->delete_search_results_template();
-			
+
 		} // end if
-		
+
 	} // end constructor
 
 	/*--------------------------------------------------------*
 	 * API Functions
 	 *--------------------------------------------------------*/
-	 
+
 	/**
 	 * Outputs the content of the widget.
 	 *
@@ -54,16 +54,16 @@ class Google_Custom_Search extends WP_Widget {
 	 * @version	3.0
 	 */
 	function widget( $args, $instance ) {
-	
+
 		extract( $args, EXTR_SKIP );
 
 		$gcse_content = empty( $instance['gcse_content'] ) ? '' : apply_filters( 'gcse_content', $instance['gcse_content'] );
-    
+
 		// Display the widget
 		include( get_template_directory() . '/lib/google-custom-search/views/widget.php' );
-		
+
 	} // end widget
-	
+
 	/**
 	 * Processes the widget's options to be saved.
 	 *
@@ -73,15 +73,15 @@ class Google_Custom_Search extends WP_Widget {
 	 * @version	3.0
 	 */
 	function update( $new_instance, $old_instance ) {
-		
+
 		$instance = $old_instance;
 
 		$instance['gcse_content'] = $new_instance['gcse_content'];
-		
+
 		return $instance;
-		
+
 	} // end widget
-	
+
 	/**
 	 * Generates the administration form for the widget.
 	 *
@@ -99,12 +99,12 @@ class Google_Custom_Search extends WP_Widget {
 		);
 
     	$gcse_content = esc_textarea( $instance['gcse_content'] );
-   
+
 		// Display the admin form
 		include( get_template_directory() . '/lib/google-custom-search/views/admin.php' );
-		
+
 	} // end form
-	
+
 	/*--------------------------------------------------------*
 	 * Helper Functions
 	 *--------------------------------------------------------*/
@@ -118,7 +118,7 @@ class Google_Custom_Search extends WP_Widget {
 	function load_admin_stylesheets() {
 		wp_enqueue_style( 'gcse-widget', get_template_directory_uri() . '/lib/google-custom-search/css/admin.css', false, STANDARD_THEME_VERSION );
 	} // end load_stylesheets
-	
+
 	/**
 	 * Loads the stylesheets for the sidebar and the page template.
 	 *
@@ -128,7 +128,7 @@ class Google_Custom_Search extends WP_Widget {
 	function load_stylesheets() {
 		wp_enqueue_style( 'gcse-widget', get_template_directory_uri() . '/lib/google-custom-search/css/widget.css', false, STANDARD_THEME_VERSION );
 	} // end load_stylesheets
-	
+
 	/**
 	 * Creates the search results page that will be used to render the results based on the search.
 	 * If a page with the 'Search Results' slug already exists, an error will be thrown.
@@ -138,13 +138,17 @@ class Google_Custom_Search extends WP_Widget {
 	 */
 	private function create_search_results_template() {
 
-		if( null == get_page_by_title( 'search results' ) ) { 
+		if( null == get_page_by_title( 'search results' ) ) {
 
 			// Get the current user
 			$current_user = wp_get_current_user();
-			
+
+			// Get the content
+			$page_content = wp_remote_get( get_template_directory_uri() . '/lib/google-custom-search/lib/Standard_Google_Custom_Search.template.php' );
+			$page_content = $page_content['body'];
+
 			// Create the page
-			$page_id = wp_insert_post(	
+			$page_id = wp_insert_post(
 				array(
 					'comment_status'	=>	'closed',
 					'ping_status'		=>	'closed',
@@ -153,26 +157,26 @@ class Google_Custom_Search extends WP_Widget {
 					'post_title'		=>	__( 'Search Results', 'standard' ),
 					'post_status'		=>	'publish',
 					'post_type'			=>	'page',
-					'post_content'		=>	wp_remote_get( get_template_directory_uri() . '/lib/google-custom-search/lib/Standard_Google_Custom_Search.template.php' )
+					'post_content'		=>	$page_content
 				)
 			);
-			
+
 			// If the value exists, delete it first. I don't want to write extra rows into the table.
 			if ( 0 == count( get_post_meta( $page_id, 'standard_google_custom_search' ) ) ) {
 				delete_post_meta( $page_id, 'standard_google_custom_search' );
 			} // end if
-			
+
 			// Mark that this post was created by Standard
 			update_post_meta( $page_id, 'standard_google_custom_search', true );
 
 		} else {
-		
+
 			$this->existing_search_results_template();
-		
+
 		} // end if
-		
+
 	} // end create_search_results_template
-	
+
 	/**
 	 * Deletes the search results page when the widget is no longer active.
 	 *
@@ -183,16 +187,16 @@ class Google_Custom_Search extends WP_Widget {
 
 		$query = new WP_Query('post_type=page&meta_key=standard_google_custom_search');
 		if( $query->have_posts() ) {
-		
+
 			$query->the_post();
 			wp_delete_post( get_the_ID(), true );
-			
+
 			wp_reset_postdata();
-			
+
 		} // end
-		
+
 	} // end delete_search_results_template
-	
+
 	/**
 	 * Renders a notification if the user already has an existing search results template.
 	 *
@@ -206,34 +210,34 @@ class Google_Custom_Search extends WP_Widget {
 
 		// If we're on the widgets page...
 		if( $this->is_page( 'widgets.php' ) ) {
-		
+
 			// ... and a search results page already exists, then we need to throw up an error.
 			if( 1 != get_post_meta( $page_id, 'standard_google_custom_search', true ) ) {
-			
+
 				echo '<div id="standard-gcse-template-exists-notification" class="updated"><p>' . __( 'Could not configure Google Custom Search widget because the required "search-results" permalink is already in use. Please rename <a href="post.php?post=' . $page_id . '&action=edit">the existing page\'s permalink</a>, or <a href="post.php?post=' . $page_id . '&action=edit">delete the page</a>, and try again.', 'standard') . '</p></div>';
-			
+
 			} // end if
-		
+
 		// Otherwise, if we're editing the generated Search Results page, then we need to display a message.
 		} else if ( isset( $_GET['post'] ) && $page_id == $_GET['post'] ) {
-		
+
 			// If the user is trying to delete the page, don't let them.
 			if( isset( $_GET['action'] ) && 'trash' == strtolower( $_GET['action'] ) ) {
-				
+
 				wp_redirect( $_SERVER['HTTP_REFERER'] );
 				exit;
-			
+
 			// Otherwise, they are on the post edit page so they should be able to see the message.
 			} else {
-			
-				echo '<div id="standard-gcse-template-notification" class="updated"><p>' . __( 'This page was generated by Standard for use with the Google Custom Search widget. <a href="widgets.php">Remove the widget</a> from all sidebars to delete this page.', 'standard' ) . '</p></div>';	
-				
+
+				echo '<div id="standard-gcse-template-notification" class="updated"><p>' . __( 'This page was generated by Standard for use with the Google Custom Search widget. <a href="widgets.php">Remove the widget</a> from all sidebars to delete this page.', 'standard' ) . '</p></div>';
+
 			} // end if
-				
-		} // end if		
+
+		} // end if
 
 	} // end existing_search_results_template
-	
+
 	/**
 	 * Determines if were on the page specified by the incoming filename.
 	 *
@@ -247,4 +251,4 @@ class Google_Custom_Search extends WP_Widget {
 	} // end is_widgets_page
 
 } // end class
-add_action( 'widgets_init', create_function( '', 'register_widget( "Google_Custom_Search" );' ) ); 
+add_action( 'widgets_init', create_function( '', 'register_widget( "Google_Custom_Search" );' ) );
--- lib/google-custom-search/views/admin.php
+++ lib/google-custom-search/views/admin.php
@@ -15,5 +15,5 @@
     	<?php _e( '<em><a href="http://docs.8bit.io/standard/widgets/google-custom-search-widget/" target="_blank">Learn More</a>.</em>', 'standard'); ?></p>
     	<input id="<?php echo $this->get_field_id( 'gcse_content' ); ?>" name="<?php echo $this->get_field_name( 'gcse_content' ); ?>" rows="10" cols="30" value="<?php echo $gcse_content; ?>" />
     </div><!-- /.option -->
-    
+
 </div><!-- /wrapper -->
