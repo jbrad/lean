00c8a52ed4584017b8e03fd64846af111551c88f
Tom McFarlin <tom@tommcfarlin.com>
2013-05-22 14:10:53 -0400

    Making the SEO title metabox consistent with what the title will show
    Fixes #514

--- lib/seo/plugin.php
+++ lib/seo/plugin.php
@@ -11,27 +11,27 @@
  * @since		3.0
  */
 class Standard_SEO {
-	 
+
 	/*--------------------------------------------*
 	 * Constructor
 	 *--------------------------------------------*/
-	
+
 	/**
 	 * Initializes the widget's classname, description, and JavaScripts.
-	 */ 
+	 */
 	function __construct() {
-	
+
 		add_action( 'admin_print_styles', array( $this, 'admin_styles' ) );
-		
+
 	    add_action( 'add_meta_boxes', array( $this, 'seo_meta_boxes' ) );
 	    add_action( 'save_post', array( $this, 'save_postdata' ) );
-	    
+
 	} // end constructor
-	
+
 	/*--------------------------------------------*
 	 * Core Functions
 	 *---------------------------------------------*/
-	
+
 	/**
  	 * Adds the Standard SEO meta box to the post and page screens in the dashboard.
  	 *
@@ -59,9 +59,9 @@ class Standard_SEO {
 		);
 
 	} // end action_method_name
-	
+
 	/**
-	 * Renders the actual Standard SEO Preview meta box and preview area to the page. 
+	 * Renders the actual Standard SEO Preview meta box and preview area to the page.
 	 *
 	 * @params	object $post	The post on which the box should be rendered.
 	 * @since	3.0
@@ -70,72 +70,72 @@ class Standard_SEO {
 	public function post_level_display( $post ) {
 
 		wp_nonce_field( plugin_basename( __FILE__ ), 'standard_seo_nonce' );
-	
+
 		$html = '<p>' . __( 'Search Results Preview ', 'standard' ) . '</p>';
 		$html .= '<div id="search-engine-preview">';
 
-			$html .= '<p id="search-results-title"><span id="post-title"></span>' . ' ' . __( '-', 'standard' ) . ' ' . '<span id="blog-title"></span></p>';
+			$html .= '<p id="search-results-title"><span id="post-title"></span>' . ' ' . __( '|', 'standard' ) . ' ' . '<span id="blog-title"></span></p>';
 			$html .= '<p id="search-results-meta"><span id="permalink"></span></p>';
-			
+
 			// Look to see if the user has the Google Profile URL specified
 			$current_user = wp_get_current_user();
 			if( '' != get_user_meta( $current_user->ID, 'google_plus', true ) ) {
-			
+
 				// Determine if the user is using a gplus.to address
 				$google_plus_url = user_trailingslashit( get_user_meta( $current_user->ID, 'google_plus', true ) );
 				if( standard_is_gplusto_url ( $google_plus_url ) ) {
-				
-					$google_plus_url = standard_get_google_plus_from_gplus( $google_plus_url ); 					
+
+					$google_plus_url = standard_get_google_plus_from_gplus( $google_plus_url );
 
 					// Read the URL into an array
 					$google_plus_id = explode( '/',  trailingslashit( $google_plus_url ) );
-					
+
 					// Note the third index of this array should always be at 3 after user_trailingslashit
 					$google_plus_id = isset( $google_plus_id[3] ) ? $google_plus_id[3] : $google_plus_id[1];
-					
+
 				// The user isn't using gplus.to, so the index of the ID is different
 				} else {
-					
+
 					// Read the URL into an array
 					$google_plus_id = explode( '/',  trailingslashit( $google_plus_url ) );
 
 					// Note the third index of this array should alwas be at 5 after user_trailingslashit
-					$google_plus_id = $google_plus_id[3];					
-				
+					$google_plus_id = $google_plus_id[3];
+
 				} // end if/else
-								
+
 				// Now create the element
 				$html .= '<p id="google-plus-avatar">';
 					$html .= '<img src="https://profiles.google.com/s2/photos/profile/' . $google_plus_id . '" alt="" width="44" height="44" />';
 				$html .= '</p>';
-				
+
 			} // end if
 
 			$html .= '<p id="search-results-meta-description"><span id="date">Date</span> - <span id="description">' . get_post_meta( $post->ID, 'standard_seo_post_meta_description', true ) . '</span></p>';
 			$html .= '<span id="site-title" class="hidden">' . get_bloginfo( 'name' ) . '</span>';
 			$html .= '<span id="todays-date" class="hidden">' . date( get_option( 'date_format' ) ) . '</span>';
-			
+
 		$html .= '</div><!-- /#search-engine-preview -->';
-		
+
 		$html .= '<div id="meta-description-container">';
-		
+
 			// The label for the meta description
 			$html .= '<p>' . __( 'Meta Description ', 'standard' ) . '(<span id="character-count">' . __( '140', 'standard' ) . '</span>' . ' ' . __( 'characters remaining)', 'standard' );'</p>';
-			
-			// The input field for the meta description			
+
+			// The input field for the meta description
 			$html .= '<textarea id="standard_seo_post_meta_description" name="standard_seo_post_meta_description" maxlength="140">' . get_post_meta( $post->ID, 'standard_seo_post_meta_description', true ) . '</textarea>';
-			
+
 			// The description for the field
 			$html .= '<p class="description">';
 				$html .= __( 'Writing a meta description for every post is strongly recommended for SEO. If not provided, no description will be published.', 'standard' );
 			$html .= '</p>';
-		
+
 		$html .= '</div><!-- /#meta-description-container -->';
-		
+
 		echo $html;
-		
+
 	} // end post-Level_display
-	
+
 	/**
 	 * Saves the post data to post defined by the incoming ID.
 	 *
@@ -144,26 +144,26 @@ class Standard_SEO {
 	 * @version	1.0
 	 */
 	public function save_postdata( $post_id ) {
-		
+
 		if( isset( $_POST['standard_seo_nonce'] ) ) {
-		
+
 			// Don't save if the user hasn't submitted the changes
 			if( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
 				return;
 			} // end if
-			
+
 			// Verify that the input is coming from the proper form
 			if( ! wp_verify_nonce( $_POST['standard_seo_nonce'], plugin_basename( __FILE__ ) ) ) {
 				return;
 			} // end if
-			
+
 			// Make sure the user has permissions to post
 			if( 'post' == $_POST['post_type']) {
 				if( ! current_user_can( 'edit_post', $post_id ) ) {
 					return;
 				} // end if
 			} // end if/else
-			
+
 			// Read the meta description
 			$meta_description = $_POST['standard_seo_post_meta_description'];
 
@@ -176,13 +176,13 @@ class Standard_SEO {
 			update_post_meta( $post_id, 'standard_seo_post_meta_description', $meta_description );
 
 		} // end if
-			
+
 	} // end save_postdata
-  
+
 	/*--------------------------------------------*
 	 * Helper Functions
 	 *---------------------------------------------*/
-	
+
 	/**
 	 * Registers and enqueues stylesheets for the administration panel.
 	 *
@@ -192,6 +192,6 @@ class Standard_SEO {
 	public function admin_styles() {
 		wp_enqueue_style( 'standard-seo-admin', get_template_directory_uri() . '/lib/seo/css/admin.css', false, STANDARD_THEME_VERSION );
 	} // end admin_styles
-  
+
 } // end class
 new Standard_SEO();
