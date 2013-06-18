fba5046792148ae228cfeac8f7e928d7df9543cd
Tom McFarlin <tom@tommcfarlin.com>
2013-06-18 12:15:51 -0400

    updating the google+ profile url description
    fixes #489

--- js/dev/admin.profiles.js
+++ js/dev/admin.profiles.js
@@ -1,23 +1,23 @@
 (function($) {
 	"use strict";
 	$(function () {
-	
+
 		var $googlePlus, sMessage, spanMessage;
 		$googlePlus = $('#google_plus');
-	
+
 		if( 0 < $googlePlus.length ) {
-			
+
 			/* Translators: This will need to be localized. */
-			sMessage = "We do not support Google+ vanity URLs.";
-			
-			spanMessage = 
+			sMessage = "Use long URL, not vanity URL.";
+
+			spanMessage =
 				$('<span />')
 					.attr('class', 'description')
 					.html( '&nbsp;' + sMessage );
-			
+
 			spanMessage.insertAfter( $googlePlus );
-			
+
 		} // end if
-		
+
 	});
 }(jQuery));
