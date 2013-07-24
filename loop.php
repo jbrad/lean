005fad1dc976d0338c05248fcf4372ba50d21824
Tom McFarlin <tom@tommcfarlin.com>
2013-07-24 18:39:18 -0400

    Apply a `more-link` class to all other anchors that are not generated by the more tag.
    Fixes #308

--- loop.php
+++ loop.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * The template for displaying standard post formats.
- * 
+ *
  * @package Standard
- * @since 	3.0
- * @version	3.1
+ * @since 	3.0.0
+ * @version	3.2.0
  */
 ?>
 
@@ -20,12 +20,12 @@
 						<?php the_post_thumbnail( 'thumbnail' );	?>
 					</a>
 				</div> <!-- /.thumbnail -->
-			<?php } // end if ?> 
-		<?php } // end if ?> 
+			<?php } // end if ?>
+		<?php } // end if ?>
 		<div class="title-wrap clearfix">
 			<?php if( '' !== get_the_title() ) { ?>
 				<?php if( is_single() || is_page() ) { ?>
-					<h1 class="post-title entry-title"><?php the_title(); ?></h1>	
+					<h1 class="post-title entry-title"><?php the_title(); ?></h1>
 				<?php } else { ?>
 					<h2 class="post-title entry-title">
 						<a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php printf( esc_attr__( '%s', 'standard' ), the_title_attribute( 'echo=0' ) ); ?>"><?php the_title(); ?></a>
@@ -52,36 +52,38 @@
 	<div id="content-<?php the_ID(); ?>" class="entry-content clearfix">
 		<?php if( ( is_category() || is_archive() || is_home() ) && has_excerpt() ) { ?>
 			<?php the_excerpt( ); ?>
-			<a href="<?php echo get_permalink(); ?>"><?php _e( 'Continue Reading...', 'standard' ); ?></a>
+			<a href="<?php echo get_permalink(); ?>" class="more-link">
+				<?php _e( 'Continue Reading...', 'standard' ); ?>
+			</a>
 		<?php } else { ?>
 			<?php the_content( __( 'Continue Reading...', 'standard' ) ); ?>
 		<?php } // end if/else ?>
-		<?php 
-			wp_link_pages( 
-				array( 
-					'before' 	=> '<div class="page-link"><span>' . __( 'Pages:', 'standard' ) . '</span>', 
-					'after' 	=> '</div>' 
-				) 
-			); 
+		<?php
+			wp_link_pages(
+				array(
+					'before' 	=> '<div class="page-link"><span>' . __( 'Pages:', 'standard' ) . '</span>',
+					'after' 	=> '</div>'
+				)
+			);
 		?>
 	</div><!-- /.entry-content -->
-	
+
 	<div class="post-meta clearfix">
 
 		<div class="meta-date-cat-tags pull-left">
-		
+
 			<?php $category_list = get_the_category_list( __( ', ', 'standard' ) ); ?>
 			<?php if( $category_list ) { ?>
 				<?php printf( '<span class="the-category">' . __( 'In %1$s', 'standard' ) . '</span>', $category_list ); ?>
 			<?php } // end if ?>
-			
+
 			<?php $tag_list = get_the_tag_list( '', __( ', ', 'standard' ) ); ?>
 			<?php if( $tag_list ) { ?>
 				<?php printf( '<span class="the-tags">' . __( '%1$s', 'standard' ) . '</span>', $tag_list ); ?>
 			<?php } // end if ?>
-			
+
 		</div><!-- /meta-date-cat-tags -->
-		
+
 		<div class="meta-comment-link pull-right">
 			<a class="pull-right post-link" href="<?php the_permalink(); ?>" title="<?php esc_attr_e( 'permalink', 'standard' ); ?>">&nbsp;<img src="<?php echo esc_url( get_template_directory_uri() . '/images/icn-permalink.png' ); ?>" alt="<?php esc_attr_e( 'permalink', 'standard' ); ?>" /></a>
 			<?php if ( '' != get_post_format() ) { ?>
