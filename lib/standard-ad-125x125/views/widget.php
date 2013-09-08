5af7ecb64287dba2283de36e2b6035fd5249e37c
jbrad <jason.bradley@me.com>
2013-09-08 09:31:08 -0500

    Change location of row class

--- lib/standard-ad-125x125/views/widget.php
+++ lib/standard-ad-125x125/views/widget.php
@@ -11,20 +11,18 @@
 <?php $global_options = get_option( 'standard_theme_global_options' ); $default_url = ''; ?>
 
 <?php echo isset( $args['before_widget'] ) ? $args['before_widget'] : ''; ?>
-	<div class="row">
-		<div class="standard-ad-row">
-			<ul class="thumbnails mobile-2">
-				<li class="col-md-6">
-					<div class="thumbnail">
-						<?php echo $this->display_ad( $ad1_src, $ad1_url, 1 ); ?>
-					</div><!--/.thumbnail -->
-				</li><!-- /.left -->
-				<li class="col-md-6">
-					<div class="thumbnail">
-						<?php echo $this->display_ad( $ad2_src, $ad2_url, 2 ); ?>
-					</div><!-- /.thumbnail -->
-				</li><!-- /.right -->
-			</ul><!-- /.thumbnails -->
-		</div><!-- /.standard-ad-row -->
-	</div><!-- /.row-fluid -->
+    <div class="standard-ad-row">
+        <ul class="thumbnails mobile-2 row">
+            <li class="col-md-6">
+                <div class="thumbnail">
+                    <?php echo $this->display_ad( $ad1_src, $ad1_url, 1 ); ?>
+                </div><!--/.thumbnail -->
+            </li><!-- /.left -->
+            <li class="col-md-6">
+                <div class="thumbnail">
+                    <?php echo $this->display_ad( $ad2_src, $ad2_url, 2 ); ?>
+                </div><!-- /.thumbnail -->
+            </li><!-- /.right -->
+        </ul><!-- /.thumbnails -->
+    </div><!-- /.standard-ad-row -->
 <?php echo isset( $args['after_widget'] ) ? $args['after_widget'] : ''; ?>
