2fd56093f68ab547d1ac9c0f94e4af8d1c769633
jbrad <jason.bradley@me.com>
2013-09-08 12:17:00 -0500

    Updated audio post format

--- loop-audio.php
+++ loop-audio.php
@@ -22,36 +22,22 @@
 				</div> <!-- /.thumbnail -->
 			<?php } // end if ?>
 		<?php } // end if ?>
-		<div class="title-wrap clearfix">
-			<?php if( '' !== get_the_title() ) { ?>
-				<?php if( is_single() || is_page() ) { ?>
-					<h1 class="post-title entry-title"><?php the_title(); ?></h1>
-				<?php } else { ?>
-					<h2 class="post-title entry-title">
-						<a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php printf( esc_attr__( '%s', 'standard' ), the_title_attribute( 'echo=0' ) ); ?>"><?php the_title(); ?></a>
-					</h2>
-				<?php } // end if ?>
-			<?php } // end if ?>
-			<div class="post-header-meta">
-				<span class="the-author author vcard">
-					<span class="fn">
-						<a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>" title="<?php echo get_the_author_meta( 'display_name' ); ?>">
-							<?php echo the_author_meta( 'display_name' ); ?>
-						</a>
-					</span><!-- /.fn -->
-					&nbsp;&mdash;&nbsp;
-				</span><!-- /.the-author -->
-				<?php if( strlen( trim( get_the_title() ) ) == 0 ) { ?>
-					<a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php printf( esc_attr__( '%s', 'standard' ), the_title_attribute( 'echo=0' ) ); ?>"><span class="the-time updated"><?php the_time( get_option( 'date_format' ) ); ?></span></a>
-				<?php } else { ?>
-					<span class="the-time updated"><?php the_time( get_option( 'date_format' ) ); ?></span>
-				<?php } // end if/else ?>
-				<?php if( comments_open() ) { ?>
-					<span class="the-comment-link">&mdash;&nbsp;<?php comments_popup_link( __( 'Leave a comment', 'standard' ), __( '1 Comment', 'standard' ), __( '% Comments', 'standard' ), '', '' ); ?></span>
-				<?php } // end if ?>
-			</div><!-- /.post-header-meta -->
+		<div class="title-wrap clearfix row">
+            <div class="col-sm-1 hidden-xs">
+                <span class="icon-music"></span>
+            </div> <!-- /.col-md-1 -->
+            <div class="col-sm-11 col-xs-12">
+                <?php if( '' !== get_the_title() ) { ?>
+                    <?php if( is_single() || is_page() ) { ?>
+                        <h1 class="post-title entry-title"><?php the_title(); ?></h1>
+                    <?php } else { ?>
+                        <h2 class="post-title entry-title">
+                            <a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php printf( esc_attr__( '%s', 'standard' ), the_title_attribute( 'echo=0' ) ); ?>"><?php the_title(); ?></a>
+                        </h2>
+                    <?php } // end if ?>
+                <?php } // end if ?>
+            </div> <!-- /.col-md-11 -->
 		</div><!-- /.title-wrap -->
-
 	</div><!-- /.post-header -->
 
 	<div id="content-<?php the_ID(); ?>" class="entry-content clearfix">
@@ -71,28 +57,35 @@
 		?>
 	</div><!-- /.entry-content -->
 
-	<div class="post-meta clearfix">
+    <div class="post-meta clearfix">
 
-		<div class="meta-date-cat-tags pull-left">
+        <div class="meta-date-cat-tags pull-left">
 
-			<?php $category_list = get_the_category_list( __( ', ', 'standard' ) ); ?>
-			<?php if( $category_list ) { ?>
-				<?php printf( '<span class="the-category">' . __( 'In %1$s', 'standard' ) . '</span>', $category_list ); ?>
-			<?php } // end if ?>
+            <?php if( is_multi_author() ) { ?>
+                <span class="the-author">&nbsp;<?php _e( 'Posted by', 'standard' ); ?>&nbsp;<a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>" title="<?php echo get_the_author_meta( 'display_name' ); ?>"><?php echo the_author_meta( 'display_name' ); ?></a></span>
+                <span class="the-time updated">&nbsp;<?php _e( 'on ', 'standard' ) . ' '; echo get_the_time( get_option( 'date_format' ) ); ?></span>
+            <?php } else { ?>
+                <?php printf( '<span class="the-time updated">' . __( 'Posted on %1$s', 'standard' ) . '</span>', get_the_time( get_option( 'date_format' ) ) ); ?>
+            <?php } // end if ?>
 
-			<?php $tag_list = get_the_tag_list( '', __( ', ', 'standard' ) ); ?>
-			<?php if( $tag_list ) { ?>
-				<?php printf( '<span class="the-tags">' . __( '%1$s', 'standard' ) . '</span>', $tag_list ); ?>
-			<?php } // end if ?>
+            <?php $category_list = get_the_category_list( __( ', ', 'standard' ) ); ?>
+            <?php if( $category_list ) { ?>
+                <?php printf( '<span class="the-category">' . __( 'In %1$s', 'standard' ) . '</span>', $category_list ); ?>
+            <?php } // end if ?>
 
-		</div><!-- /meta-date-cat-tags -->
+            <?php $tag_list = get_the_tag_list( '', __( ', ', 'standard' ) ); ?>
+            <?php if( $tag_list ) { ?>
+                <?php printf( '<span class="icon-tags"></span> ' . __( '%1$s', 'standard' ) . '</span>', $tag_list ); ?>
+            <?php } // end if ?>
 
-		<div class="meta-comment-link pull-right">
-			<a class="pull-right post-link" href="<?php the_permalink(); ?>" title="<?php esc_attr_e( 'permalink', 'standard' ); ?>">&nbsp;<img src="<?php echo esc_url( get_template_directory_uri() . '/images/icn-permalink.png' ); ?>" alt="<?php esc_attr_e( 'permalink', 'standard' ); ?>" /></a>
-			<?php if ( '' != get_post_format() ) { ?>
-				<span class="the-comment-link"><?php comments_popup_link( __( 'Leave a comment', 'standard' ), __( '1 Comment', 'standard' ), __( '% Comments', 'standard' ), '', ''); ?></span>
-			<?php } // end if ?>
-		</div><!-- /meta-comment-link -->
+        </div><!-- /meta-date-cat-tags -->
 
-	</div><!-- /.post-meta -->
+        <div class="meta-comment-link pull-right">
+            <a class="pull-right post-link" href="<?php the_permalink(); ?>" title="<?php esc_attr_e( 'permalink', 'standard' ); ?>">&nbsp;<span class="icon-link"></span></a>
+            <?php if ( '' != get_post_format() ) { ?>
+                <span class="the-comment-link"><?php comments_popup_link( __( 'Leave a comment', 'standard' ), __( '1 Comment', 'standard' ), __( '% Comments', 'standard' ), '', ''); ?></span>
+            <?php } // end if ?>
+        </div><!-- /meta-comment-link -->
+
+    </div><!-- /.post-meta -->
 </div><!-- /#post -->
