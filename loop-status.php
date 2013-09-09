65172ca48e2dc7304b80e0200c6ed81e3e84cc97
jbrad <jason.bradley@me.com>
2013-09-09 07:31:46 -0500

    Fixed row and col for loop-status

--- loop-status.php
+++ loop-status.php
@@ -12,11 +12,11 @@
 <div id="post-<?php the_ID(); ?>" <?php post_class( 'post format-status' ); ?>>
 
 	<div class="post-header clearfix">
-		<div class="row-fluid">
-				<div class="post-avatar span2">
+		<div class="row">
+				<div class="post-avatar col-md-2">
 					<?php echo get_avatar( get_the_author_meta( 'ID' ), 80 ); ?>
 				</div><!-- /.post-avatar -->
-			<div class="entry-content span10 clearfix">
+			<div class="entry-content col-md-10 clearfix">
 				<?php if( ( is_category() || is_archive() || is_home() ) && has_excerpt() ) { ?>
 					<?php the_excerpt( ); ?>
 					<a href="<?php echo get_permalink(); ?>"><?php _e( 'Continue Reading...', 'standard' ); ?></a>
@@ -24,7 +24,7 @@
 					<?php the_content( __( 'Continue Reading...', 'standard' ) ); ?>
 				<?php } // end if/else ?>
 			</div><!-- /.entry-content -->
-		</div><!-- /row-fluid -->
+		</div><!-- /row -->
 	</div> <!-- /.post-header -->
 					
 	<div class="post-meta clearfix">
