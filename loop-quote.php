3143b8db0c7abdd139b1c66bce1cae6af0f67a9c
jbrad <jason.bradley@me.com>
2013-09-15 10:15:17 -0500

    Refactor loop-quote.php to remove standard and use lean

--- loop-quote.php
+++ loop-quote.php
@@ -19,9 +19,9 @@
                 <div class="col-sm-11 col-xs-12">
 				<?php if( ( is_category() || is_archive() || is_home() ) && has_excerpt() ) { ?>
 					<?php the_excerpt( ); ?>
-					<a href="<?php echo get_permalink(); ?>"><?php _e( 'Continue Reading...', 'standard' ); ?></a>
+					<a href="<?php echo get_permalink(); ?>"><?php _e( 'Continue Reading...', 'lean' ); ?></a>
 				<?php } else { ?>
-					<?php the_content( __( 'Continue Reading...', 'standard' ) ); ?>
+					<?php the_content( __( 'Continue Reading...', 'lean' ) ); ?>
 				<?php } // end if/else ?>
                 </div> <!-- /.col-md-11 -->
 			</div><!-- /.entry-content -->
@@ -34,7 +34,7 @@
 				<h1 class="post-title entry-title"><?php the_title(); ?></h1>	
 			<?php } else { ?>
 				<h2 class="post-title entry-title">
-					<a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php printf( esc_attr__( '%s', 'standard' ), the_title_attribute( 'echo=0' ) ); ?>"><?php the_title(); ?></a>
+					<a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php printf( esc_attr__( '%s', 'lean' ), the_title_attribute( 'echo=0' ) ); ?>"><?php the_title(); ?></a>
 				</h2>
 			<?php } // end if ?>
 		<?php } // end if ?>
@@ -42,7 +42,7 @@
 		<?php 
 			wp_link_pages( 
 				array( 
-					'before' => '<div class="page-link"><span>' . __( 'Pages:', 'standard' ) . '</span>', 
+					'before' => '<div class="page-link"><span>' . __( 'Pages:', 'lean' ) . '</span>',
 					'after' => '</div>' 
 				) 
 			); 
@@ -55,28 +55,28 @@
 			<div class="meta-date-cat-tags pull-left">
 			
 				<?php if( is_multi_author() ) { ?>
-					<span class="the-author">&nbsp;<?php _e( 'Posted by', 'standard' ); ?>&nbsp;<a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>" title="<?php echo get_the_author_meta( 'display_name' ); ?>"><?php echo the_author_meta( 'display_name' ); ?></a></span>
-					<span class="the-time updated">&nbsp;<?php _e( 'on ', 'standard' ) . ' '; echo get_the_time( get_option( 'date_format' ) ); ?></span>
+					<span class="the-author">&nbsp;<?php _e( 'Posted by', 'lean' ); ?>&nbsp;<a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>" title="<?php echo get_the_author_meta( 'display_name' ); ?>"><?php echo the_author_meta( 'display_name' ); ?></a></span>
+					<span class="the-time updated">&nbsp;<?php _e( 'on ', 'lean' ) . ' '; echo get_the_time( get_option( 'date_format' ) ); ?></span>
 				<?php } else { ?>
-					<?php printf( '<span class="the-time updated">' . __( 'Posted on %1$s', 'standard' ) . '</span>', get_the_time( get_option( 'date_format' ) ) ); ?>
+					<?php printf( '<span class="the-time updated">' . __( 'Posted on %1$s', 'lean' ) . '</span>', get_the_time( get_option( 'date_format' ) ) ); ?>
 				<?php } // end if ?>
 			
-				<?php $category_list = get_the_category_list( __( ', ', 'standard' ) ); ?>
+				<?php $category_list = get_the_category_list( __( ', ', 'lean' ) ); ?>
 				<?php if( $category_list ) { ?>
-					<?php printf( '<span class="the-category">' . __( 'In %1$s', 'standard' ) . '</span>', $category_list ); ?>
+					<?php printf( '<span class="the-category">' . __( 'In %1$s', 'lean' ) . '</span>', $category_list ); ?>
 				<?php } // end if ?>
 				
-				<?php $tag_list = get_the_tag_list( '', __( ', ', 'standard' ) ); ?>
+				<?php $tag_list = get_the_tag_list( '', __( ', ', 'lean' ) ); ?>
 				<?php if( $tag_list ) { ?>
-                    <?php printf( '<span class="icon-tags"></span> ' . __( '%1$s', 'standard' ) . '</span>', $tag_list ); ?>
+                    <?php printf( '<span class="icon-tags"></span> ' . __( '%1$s', 'lean' ) . '</span>', $tag_list ); ?>
 				<?php } // end if ?>
 				
 			</div><!-- /meta-date-cat-tags -->
 			
 			<div class="meta-comment-link pull-right">
-				<a class="pull-right post-link" href="<?php the_permalink(); ?>" title="<?php esc_attr_e( 'permalink', 'standard' ); ?>">&nbsp;<span class="icon-link"></span></a>
+				<a class="pull-right post-link" href="<?php the_permalink(); ?>" title="<?php esc_attr_e( 'permalink', 'lean' ); ?>">&nbsp;<span class="icon-link"></span></a>
 				<?php if ( '' != get_post_format() ) { ?>
-					<span class="the-comment-link"><?php comments_popup_link( __( 'Leave a comment', 'standard' ), __( '1 Comment', 'standard' ), __( '% Comments', 'standard' ), '', ''); ?></span>
+					<span class="the-comment-link"><?php comments_popup_link( __( 'Leave a comment', 'lean' ), __( '1 Comment', 'lean' ), __( '% Comments', 'lean' ), '', ''); ?></span>
 				<?php } // end if ?>
 			</div><!-- /meta-comment-link -->
 
