4b71b6eee4cb538cf7b7ef25b92f3c68601a4d61
jbrad <jason.bradley@me.com>
2013-09-26 17:10:41 -0500

    Adding the admin panel images back in

--- lib/google-custom-search/views/admin.php
+++ lib/google-custom-search/views/admin.php
@@ -12,7 +12,7 @@
 
     <div class="option">
     	<p><?php _e( 'Paste your Google Custom Search Engine ID here. ', 'lean' ); ?>
-    	<?php _e( '<em><a href="http://docs.8bit.io/lean/widgets/google-custom-search-widget/" target="_blank">Learn More</a>.</em>', 'lean'); ?></p>
+    	<?php _e( '<em><a href="http://docs.lean.com/widgets/google-custom-search-widget/" target="_blank">Learn More</a>.</em>', 'lean'); ?></p>
     	<input id="<?php echo $this->get_field_id( 'gcse_content' ); ?>" name="<?php echo $this->get_field_name( 'gcse_content' ); ?>" rows="10" cols="30" value="<?php echo $gcse_content; ?>" />
     </div><!-- /.option -->
 
