44b185a12dc5bd79433faa71618b0d87f29ecd42
jbrad <jason.bradley@me.com>
2013-09-26 18:05:51 -0500

    Updated homepage and added styling

---
+++ template-home.php
@@ -0,0 +1,184 @@
+<?php
+/**
+ * Template Name: Home Template
+ *
+ * The template for rendering pages without sidebars.
+ *
+ * @package Lean
+ * @version	1.0
+ * @since 	1.0
+ */
+?>
+<?php get_header(); ?>
+
+    <div id="wrapper">
+        <div class="container">
+            <div class="row">
+                <section id="main" class="col-md-12 clearfix" role="main">
+                    <div class="jumbotron">
+                        <div class="container">
+                            <h1>Lean Theme</h1>
+                            <p>Lean has been forked from Standard with extras. It's designed for bloggers, designers, and developers.</p>
+                            <a class="btn btn-primary btn-lg" href="/purchase">Purchase</a>
+                        </div>
+                    </div>
+                    <div id="features" class="container">
+                        <div class="row">
+                            <div id="bootstrap" class="col-lg-4">
+                                <h2>
+                                    <span class="icon-stack">
+                                      <i class="icon-sign-blank icon-stack-base"></i>
+                                      <i class="icon-light">B</i>
+                                    </span> Bootstrap 3
+                                </h2>
+                                <p>Lean has been updated to use the latest version of Bootstrap 3. If you are a designer or developer, you will be able to use Bootstrap to extend the theme.</p>
+                            </div> <!-- /#bootstrap -->
+                            <div id="font-awesome" class="col-lg-4">
+                                <h2>
+                                    <span class="icon-stack">
+                                      <i class="icon-sign-blank icon-stack-base"></i>
+                                      <i class="icon-flag icon-light"></i>
+                                    </span> Font Awesome
+                                </h2>
+                                <p>Lean comes with Font Awesome built in. Lean utilizes it to use for the icons in the theme for performance. It's a font, you can easily customize them yourself!</p>
+                            </div> <!-- /#font-awesome -->
+                            <div id="grunt-bower" class="col-lg-4">
+                                <h2>
+                                    <span class="icon-stack">
+                                      <i class="icon-sign-blank icon-stack-base"></i>
+                                      <i class="icon-terminal icon-light"></i>
+                                    </span> Grunt, Bower
+                                </h2>
+                                <p>Lean uses the latest technologies to pull in third-party libraries with Bower and build the theme with Grunt. It's all ready for you to customize!</p>
+                            </div> <!-- /#grunt-bower -->
+                        </div> <!-- /.row -->
+                        <div class="row">
+                            <div id="standard" class="col-lg-4">
+                                <h2>
+                                    <span class="icon-stack">
+                                      <i class="icon-sign-blank icon-stack-base"></i>
+                                      <i class="icon-light">S</i>
+                                    </span> Standard
+                                </h2>
+                                <p>Lean has been forked from Standard theme. All of the features of Standard are included and have been updated for Bootstrap 3 and HTML5.</p>
+                            </div> <!-- /#bootstrap -->
+                            <div id="seo" class="col-lg-4">
+                                <h2>
+                                    <span class="icon-stack">
+                                      <i class="icon-sign-blank icon-stack-base"></i>
+                                      <i class="icon-google-plus icon-light"></i>
+                                    </span> SEO
+                                </h2>
+                                <p>Lean has built in SEO thanks to Standard. There's no need for additional plugins. There's also support for Google+ authorship built in.</p>
+                            </div> <!-- /#font-awesome -->
+                            <div id="html5" class="col-lg-4">
+                                <h2>
+                                    <span class="icon-stack">
+                                      <i class="icon-sign-blank icon-stack-base"></i>
+                                      <i class="icon-html5 icon-light"></i>
+                                    </span> HTML5
+                                </h2>
+                                <p>Lean has full HTML5 support. All of the theme files and widgets are using the newest HTML elements like header, nav, section, aside, and articles. </p>
+                            </div> <!-- /#grunt-bower -->
+                        </div> <!-- /.row -->
+
+                        <div id="post-formats" class="row">
+                            <h3 class="col-lg-2">Post Formats</h3>
+                            <div id="gallery-post-format" class="col-lg-1">
+                                <a href="http://leantheme.co/post-format-gallery/">
+                                    <h4>
+                                        <span class="icon-stack">
+                                          <i class="icon-sign-blank icon-stack-base"></i>
+                                          <i class="icon-picture icon-light"></i>
+                                        </span> Gallery
+                                    </h4>
+                                </a>
+                            </div> <!-- /#bootstrap -->
+                            <div id="audio-post-format" class="col-lg-1">
+                                <a href="http://leantheme.co/post-format-audio/">
+                                    <h4>
+                                        <span class="icon-stack">
+                                          <i class="icon-sign-blank icon-stack-base"></i>
+                                          <i class="icon-music icon-light"></i>
+                                        </span> Audio
+                                    </h4>
+                                </a>
+                            </div> <!-- /#audio-post-format -->
+                            <div id="chat-post-format" class="col-lg-1">
+                                <a href="http://leantheme.co/post-format-chat/">
+                                    <h4>
+                                        <span class="icon-stack">
+                                          <i class="icon-sign-blank icon-stack-base"></i>
+                                          <i class="icon-comments icon-light"></i>
+                                        </span> Chat
+                                    </h4>
+                                </a>
+                            </div> <!-- /#chat-post-format -->
+                            <div id="video-post-format" class="col-lg-1">
+                                <a href="http://leantheme.co/post-format-video/">
+                                    <h4>
+                                        <span class="icon-stack">
+                                          <i class="icon-sign-blank icon-stack-base"></i>
+                                          <i class="icon-facetime-video icon-light"></i>
+                                        </span> Video
+                                    </h4>
+                                </a>
+                            </div> <!-- /#chat-post-format -->
+                            <div id="quote-post-format" class="col-lg-1">
+                                <a href="http://leantheme.co/post-format-quote/">
+                                    <h4>
+                                        <span class="icon-stack">
+                                          <i class="icon-sign-blank icon-stack-base"></i>
+                                          <i class="icon-quote-left icon-light"></i>
+                                        </span> Quote
+                                    </h4>
+                                </a>
+                            </div> <!-- /#quote-post-format -->
+                            <div id="image-post-format" class="col-lg-1">
+                                <a href="http://leantheme.co/post-format-image/">
+                                    <h4>
+                                        <span class="icon-stack">
+                                          <i class="icon-sign-blank icon-stack-base"></i>
+                                          <i class="icon-camera icon-light"></i>
+                                        </span> Image
+                                    </h4>
+                                </a>
+                            </div> <!-- /#image-post-format -->
+                            <div id="status-post-format" class="col-lg-1">
+                                <a href="http://leantheme.co/post-format-status/">
+                                    <h4>
+                                        <span class="icon-stack">
+                                          <i class="icon-sign-blank icon-stack-base"></i>
+                                          <i class="icon-thumbs-up icon-light"></i>
+                                        </span> Status
+                                    </h4>
+                                </a>
+                            </div> <!-- /#image-post-format -->
+                            <div id="link-post-format" class="col-lg-1">
+                                <a href="http://leantheme.co/post-format-link/">
+                                    <h4>
+                                        <span class="icon-stack">
+                                          <i class="icon-sign-blank icon-stack-base"></i>
+                                          <i class="icon-link icon-light"></i>
+                                        </span> Link
+                                    </h4>
+                                </a>
+                            </div> <!-- /#link-post-format -->
+                            <div id="aside-post-format" class="col-lg-1">
+                                <a href="http://leantheme.co/post-format-aside/">
+                                    <h4>
+                                        <span class="icon-stack">
+                                          <i class="icon-sign-blank icon-stack-base"></i>
+                                          <i class="icon-calendar icon-light"></i>
+                                        </span> Aside
+                                    </h4>
+                                </a>
+                            </div> <!-- /#aside-post-format -->
+                        </div> <!-- /.row -->
+                    </div> <!-- /#features -->
+
+                </section><!-- /#main -->
+            </div><!--/row -->
+        </div><!-- /container -->
+    </div> <!-- /#wrapper -->
+<?php get_footer(); ?>
