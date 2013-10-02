e9c44723e44f020e2161190ada99b749905a27ed
jbrad <jason.bradley@me.com>
2013-10-01 21:12:13 -0500

    Refactored the theme name to be in one place

--- js/dev/admin.menu.js
+++ js/dev/admin.menu.js
@@ -1,16 +1,17 @@
 (function ($) {
 	"use strict";
 	$(function () {
+
+        var themeName = 'Lean',
+            aActiveTab,
+            sActiveTab;
 	
 		// Hide the 'Lean' item under the custom menu
-		$('.wp-submenu-head:contains("Lean")')
+		$('.wp-submenu-head:contains("' + themeName +'")')
 			.next()
 			.children(':first')
 			.hide();
 			
-		// Mark the proper list item as active
-		var aActiveTab, sActiveTab;
-		
 		aActiveTab = document.location.href.split('tab=');
 		if(aActiveTab !== undefined && aActiveTab !== null && aActiveTab.length > 1) {
 		
@@ -40,7 +41,7 @@
 
 			// If they click the top-level menu item, we'll default to Global
 			/* Translators: This will need to be localized. */
-			if($('a.wp-menu-open').text().toLowerCase() === 'lean') {
+			if($('a.wp-menu-open').text().toLowerCase() === themeName.toLowerCase()) {
 				$('a:contains("Global")').parent().addClass('current');
 			} // end if
 
