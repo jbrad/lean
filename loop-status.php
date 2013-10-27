b25182fb924fe9e4c300eb0c6e6cd0ac16a4326c
jbrad <jason.bradley@me.com>
2013-10-27 17:17:14 -0500

    Resolves #19

--- loop-status.php
+++ loop-status.php
@@ -13,10 +13,10 @@
 
 	<div class="post-header clearfix">
 		<div class="row">
-				<div class="post-avatar col-md-2">
+				<div class="post-avatar col-md-2 col-sm-4 col-xs-12">
 					<?php echo get_avatar( get_the_author_meta( 'ID' ), 80 ); ?>
 				</div><!-- /.post-avatar -->
-			<div class="entry-content col-md-10 clearfix">
+			<div class="entry-content col-md-10 col-sm-8 col-xs-12 clearfix">
                 <?php get_template_part( 'includes/loop.post-content' ); ?>
 			</div><!-- /.entry-content -->
 		</div><!-- /row -->
