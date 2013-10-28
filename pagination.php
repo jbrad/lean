ad54289763bc2ffe154c3325b4d009b5016985ea
jbrad <jason.bradley@me.com>
2013-10-28 08:48:36 -0500

    Fix issue with pagination

--- pagination.php
+++ pagination.php
@@ -3,7 +3,7 @@
  * The template for providing pagination throughout the theme.
  *
  * @package lean
- * @version	1.1
+ * @version	1.4
  * @since 	1.0
  */
 ?>
@@ -11,43 +11,43 @@
 
 <?php if( is_single() && 'post' == get_post_type() ) { ?>
 
-	<nav id="single-post-nav">
-		<ul class="pager">
+    <nav id="single-post-nav">
+        <ul class="pager">
 
-			<?php $trunc_limit = 30; ?>
+            <?php $trunc_limit = 30; ?>
 
-			<?php if( '' != get_previous_post() ) { ?>
-				<li class="previous">
-					<?php previous_post_link( '<span class="previous-page">%link</span>', __( '<span class="fa fa-chevron-left"></span>', TRANSLATION_KEY ) . '&nbsp;' . truncate_text( get_previous_post()->post_title, $trunc_limit ) ); ?>
-				</li>
-			<?php } // end if ?>
+            <?php if( '' != get_previous_post() ) { ?>
+                <li class="previous">
+                    <?php previous_post_link( '<span class="previous-page">%link</span>', __( '<i class="fa fa-chevron-left"></i>', TRANSLATION_KEY ) . '&nbsp;' . truncate_text( get_previous_post()->post_title, $trunc_limit ) ); ?>
+                </li>
+            <?php } // end if ?>
 
-			<?php if( '' != get_next_post() ) { ?>
-				<li class="next">
-				<?php next_post_link( '<span class="no-previous-page-link next-page">%link</span>', '&nbsp;' . truncate_text( get_next_post()->post_title, $trunc_limit ) . __( '<span class="fa fa-chevron-right"></span>', TRANSLATION_KEY ) ); ?>
-				</li>
-			<?php } // end if ?>
-		</ul>
-	</nav><!-- /#single-post-nav -->
+            <?php if( '' != get_next_post() ) { ?>
+                <li class="next">
+                    <?php next_post_link( '<span class="no-previous-page-link next-page">%link</span>', truncate_text( get_next_post()->post_title, $trunc_limit ) . __( '&nbsp;<i class="fa fa-chevron-right"></i>', TRANSLATION_KEY ) ); ?>
+                </li>
+            <?php } // end if ?>
+        </ul>
+    </nav><!-- /#single-post-nav -->
 
 <?php } elseif ( $wp_query->max_num_pages > 1 && ( is_home() || is_archive() || is_search() ) ) { ?>
 
-	<nav id="post-nav">
-		<ul class="pager">
+    <nav id="post-nav">
+        <ul class="pager">
 
-			<?php if( get_next_posts_link() ) { ?>
-				<li class="previous">
-					<?php next_posts_link( __( '<span class="nav-previous meta-nav"><span class="fa fa-chevron-left"></span> Older</span>', TRANSLATION_KEY ) ); ?>
-				</li>
-			<?php } // end if ?>
+            <?php if( get_next_posts_link() ) { ?>
+                <li class="previous">
+                    <?php next_posts_link( __( '<span class="nav-previous meta-nav"><i class="fa fa-chevron-left"></i> Older</i>', TRANSLATION_KEY ) ); ?>
+                </li>
+            <?php } // end if ?>
 
-			<?php if( get_previous_posts_link() ) { ?>
-				<li class="next">
-					<?php previous_posts_link( __( '<span class="nav-next meta-nav">Newer <span class="fa fa-chevron-right"></span>', TRANSLATION_KEY ) ); ?>
-				</li>
-			<?php } // end if ?>
+            <?php if( get_previous_posts_link() ) { ?>
+                <li class="next">
+                    <?php previous_posts_link( __( '<span class="nav-next meta-nav">Newer <i class="fa fa-chevron-right"></i>', TRANSLATION_KEY ) ); ?>
+                </li>
+            <?php } // end if ?>
 
-		</ul><!-- /.pager -->
-	</nav><!-- /#post-nav -->
+        </ul><!-- /.pager -->
+    </nav><!-- /#post-nav -->
 
 <?php } // end if/else ?>
