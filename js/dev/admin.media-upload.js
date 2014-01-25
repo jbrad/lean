17cec1a4405992762241ed4202cade0a998b6cc1
jbrad <jason.bradley@me.com>
2014-01-25 00:40:48 -0600

    Creating a helper function for launching the media uploader

--- js/dev/admin.media-upload.js
+++ js/dev/admin.media-upload.js
@@ -1,11 +1,21 @@
 var views = {};
 
+function launchMediaUploader(title) {
+    return wp.media.frames.file_frame = wp.media({
+        title: 'Select ' + title,
+        button: {
+            text: 'Select ' + title
+        },
+        multiple: false
+    });
+}
+
 (function ($) {
     "use strict";
 
     $(function() {
 
-        $('.button.upload, .preview_image').on('click', function( event ){
+        $('.button.upload').on('click', function( event ){
             var tableRow = $(this).parents('tr'),
                 title = tableRow.find('th').html(),
                 hiddenInput = tableRow.find('input[type=hidden]'),
@@ -18,29 +28,20 @@ var views = {};
 
             if ( !file_frame ) {
 
-                file_frame = wp.media.frames.file_frame = wp.media({
-                    title: 'Select ' + title,
-                    button: {
-                        text: 'Select ' + title
-                    },
-                    multiple: false,
-                    library: {
-                        type: 'image'
-                    },
-                    displaySettings: true
-                });
-
-                file_frame.on( 'select', function() {
-                    var attachment = file_frame.state().get('selection').first().toJSON();
-                    hiddenInput.val(attachment.url);
-                    previewImage.attr('src', attachment.url).show();
-                    deleteButton.show();
-                });
+                file_frame = launchMediaUploader(title);
 
                 file_frame.on('ready', function() {
                     $('.media-sidebar').hide();
                     $('.attachments').css('right', 0);
                     $('.media-toolbar').css('right', 20);
+                });
+
+                file_frame.on( 'select', function() {
+                    var attachment = file_frame.state().get('selection').first().toJSON();
+
+                    hiddenInput.val(attachment.url);
+                    previewImage.attr('src', attachment.url).show();
+                    deleteButton.show();
                 });
 
                 views[viewId] = file_frame;
