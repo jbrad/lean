9cc865b435374560bdcf46cd6e679b22f1fa23ae
jbrad <jason.bradley@me.com>
2014-03-16 19:23:53 -0500

    Updating footer to use navbar and added new footer credits

--- footer.php
+++ footer.php
@@ -7,71 +7,73 @@
  * @since 	1.0
  */
 ?>
+<?php $presentation_options = get_option( 'theme_presentation_options' ); ?>
+<?php $dark = 'dark' == $presentation_options['contrast']; ?>
 
-<footer id="footer" class="clearfix">
+<footer>
 
     <?php if( ! is_offline() || is_user_logged_in() ) { ?>
         <?php if( is_active_sidebar( 'sidebar-3' ) || is_active_sidebar( 'sidebar-4' ) || is_active_sidebar( 'sidebar-5' ) ) { ?>
-            <div id="footer-widgets" class="clearfix">
+            <div class="jumbotron">
                 <div class="container">
                     <div class="row">
 
-                        <div id="left-footer-widgets" class="col-md-4 clearfix">
+                        <div class="footer-widget col-md-4">
                             <?php dynamic_sidebar( 'sidebar-3' ); ?>
-                        </div><!-- /#left-footer-widget -->
+                        </div><!-- /.footer-widget  -->
 
-                        <div id="center-footer-widgets" class="col-md-4 clearfix">
+                        <div class="footer-widget  col-md-4">
                             <?php dynamic_sidebar( 'sidebar-4' ); ?>
-                        </div><!-- /#center-footer-widget -->
+                        </div><!-- /.footer-widget  -->
 
-                        <div id="right-footer-widgets" class="col-md-4 clearfix">
+                        <div class="footer-widget  col-md-4">
                             <?php dynamic_sidebar( 'sidebar-5' ); ?>
-                        </div><!-- /#right-footer-widget -->
+                        </div><!-- /.footer-widget  -->
 
                     </div><!-- /row -->
                 </div><!-- /container -->
-            </div><!-- /#footer-widgets -->
+            </div><!-- /.jumbotron -->
         <?php } // end if ?>
     <?php } // end if ?>
 
-    <div id="sub-floor" class="clearfix">
+    <nav id="footer-menu" class="menu-navigation navbar-fixed-bottom navbar navbar-default <?php echo $dark ? ' navbar-inverse' : ''; ?>" role="navigation">
         <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <div id="footer-links" class="pull-left">
-                        <?php
-                        if( has_nav_menu( 'footer_menu' ) && ( ! is_offline() || is_user_logged_in() ) ) {
-                            wp_nav_menu(
-                                array(
-                                    'theme_location'  	=> 'footer_menu',
-                                    'container_class' 	=> 'menu-footer-nav-container navbar',
-                                    'items_wrap'      	=> '<ul id="%1$s" class="nav navbar-nav %2$s">%3$s</ul>',
-                                    'fallback_cb'		=> false,
-                                    'depth'          	=> 1
-                                )
-                            );
-                        } // end if
-                        ?>
-                    </div><!-- /#footer-links -->
 
-                    <?php $global_options = get_option( 'theme_global_options' ); ?>
-                    <div id="credit" class="pull-right">
+                <?php
+                wp_nav_menu(
+                    array(
+                        'container_class'	=> 'menu-header-container',
+                        'theme_location'  	=> 'footer_menu',
+                        'items_wrap'      	=> '<ul id="%1$s" class="nav navbar-nav %2$s">%3$s</ul>',
+                        'fallback_cb'	  	=> null,
+                        'walker'			=> new Bootstrap_Nav_Walker()
+                    )
+                );
+                ?>
+
+                <?php $global_options = get_option( 'theme_global_options' ); ?>
+                <p class="navbar-text navbar-right">
+                    <?php if( $presentation_options['display_footer_credits'] ) { ?>
+                            <?php printf( __( '&copy; %1$s %2$s', TRANSLATION_KEY ), date( 'Y' ), '<a class="navbar-link" href="' . home_url() . '">' . get_bloginfo( 'name' ) . '</a>'); ?>
+                    <?php } else { ?>
+
                         <?php
-                        $jasonbradley_url = 'http://jasonbradley.me';
-                        $theme_url = THEME_URL;
+                            $jasonbradley_url = 'http://jasonbradley.me';
+                            $theme_url = THEME_URL;
                         ?>
 
                         <?php if( null != get_page_by_path( 'privacy-policy' ) && 0 != get_page_by_path( 'privacy-policy' )->ID && 'publish' == get_page_by_path( 'privacy-policy' )->post_status ) { ?>
-                            <?php printf( __( '&copy; %1$s %2$s &mdash; %3$s &mdash; %4$s by %5$s', TRANSLATION_KEY ), date( 'Y' ), '<a href="' . home_url() . '">' . get_bloginfo( 'name' ) . '</a>', '<a href="' . get_permalink( get_page_by_path( 'privacy-policy' )->ID ) . '">Privacy Policy</a>', '<a href="' . $theme_url . '" target="_blank">' . THEME_NAME . '</a>', '<a href="' . $jasonbradley_url . '" target="_blank">Jason Bradley</a>' ); ?>
+                            <?php printf( __( '&copy; %1$s %2$s &mdash; %3$s &mdash; %4$s by %5$s', TRANSLATION_KEY ), date( 'Y' ), '<a class="navbar-link" href="' . home_url() . '">' . get_bloginfo( 'name' ) . '</a>', '<a href="' . get_permalink( get_page_by_path( 'privacy-policy' )->ID ) . '">Privacy Policy</a>', '<a class="navbar-link" href="' . $theme_url . '" target="_blank">' . THEME_NAME . '</a>', '<a class="navbar-link" href="' . $jasonbradley_url . '" target="_blank">Jason Bradley</a>' ); ?>
                         <?php } else { ?>
-                            <?php printf( __( '&copy; %1$s %2$s &mdash; %3$s by %4$s', TRANSLATION_KEY ), date( 'Y' ), '<a href="' . home_url() . '">' . get_bloginfo( 'name' ) . '</a>', '<a href="' . $theme_url . '" target="_blank">' . THEME_NAME . '</a>', '<a href="' . $jasonbradley_url . '" target="_blank">Jason Bradley</a>' ); ?>
+                            <?php printf( __( '&copy; %1$s %2$s &mdash; %3$s by %4$s', TRANSLATION_KEY ), date( 'Y' ), '<a class="navbar-link" href="' . home_url() . '">' . get_bloginfo( 'name' ) . '</a>', '<a class="navbar-link" href="' . $theme_url . '" target="_blank">' . THEME_NAME . '</a>', '<a class="navbar-link" href="' . $jasonbradley_url . '" target="_blank">Jason Bradley</a>' ); ?>
                         <?php } // end if/else ?>
 
-                    </div><!-- /#credits -->
-                </div><!--/span12-->
-            </div><!-- /row -->
+                    <?php } // end if ?>
+
+                </p>
         </div><!-- /.container -->
-    </div><!-- /#sub-floor -->
+    </nav> <!-- /#menu-under-header -->
+
 </footer><!-- /#footer -->
 <?php wp_footer(); ?>
 </body>
