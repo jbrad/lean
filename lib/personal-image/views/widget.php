2993e404ebaa28631e56fe3cd342fd51969603df
jbrad <jason.bradley@me.com>
2014-03-16 19:23:16 -0500

    Updating personal image to use thumbnails

--- lib/personal-image/views/widget.php
+++ lib/personal-image/views/widget.php
@@ -10,24 +10,19 @@
 ?>
 <?php if( '' != $image_src ) { ?>	
 	<?php echo $args['before_widget']; ?>
-		<figure class="pi-pic">
-		
-			<?php if( 0 < strlen( trim( $image_url ) ) ) { ?>
-				<a href="<?php echo $image_url; ?>">
-			<?php } // end if ?>
-		
-			
-			<img src="<?php echo $image_src; ?>" alt="" />
-	
-			
-			<?php if( 0 < strlen( trim( $image_url ) ) ) { ?>
-				</a>
-			<?php } // end if ?>
-		</figure><!-- /.pi-pic -->
-		
-		<?php if( '' != trim( $image_description ) ) { ?>
-			<p class="pi-bio"><?php echo $image_description; ?></p>
-		<?php } // end if ?>
-		
+    <div class="personal-image thumbnail">
+        <?php if( 0 < strlen( trim( $image_url ) ) ) { ?>
+            <a href="<?php echo $image_url; ?>">
+        <?php } // end if ?>
+
+        <img src="<?php echo $image_src; ?>" alt="" />
+
+        <?php if( 0 < strlen( trim( $image_url ) ) ) { ?>
+            </a>
+        <?php } // end if ?>
+        <div class="caption">
+            <?php echo $image_description; ?>
+        </div>
+    </div>
 	<?php echo $args['after_widget']; ?>
 <?php } // end if ?>
