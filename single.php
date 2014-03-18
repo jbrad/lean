da5fc41e2a22071d1cefd0d652e9167f2dbf8d7f
jbrad <jason.bradley@me.com>
2014-03-17 23:26:14 -0500

    Putting social links in author box inside of ul

--- single.php
+++ single.php
@@ -33,49 +33,68 @@ if( 1 == $singlePostFullWidth ) {
 
                             <?php get_template_part( 'pagination '); ?>
 
+                            <div class="sharedaddy sd-block sd-social sd-gplus">
+                                <h3 class="sd-title">Google+</h3>
+                                <div class="sd-content">
+                                    <a href="https://plus.google.com/+JasonBradley70">
+                                        <img src="https://lh5.googleusercontent.com/-yCuzeh5zIKg/AAAAAAAAAAI/AAAAAAAAADE/1GHFiQBu5t8/photo.jpg?sz=40" alt="Jason Bradley" width="20" height="20" align="absmiddle" scale="0">
+                                    </a>
+                                    <a rel="author" href="https://plus.google.com/+JasonBradley70" class="gplus-profile">Jason Bradley</a>
+                                    <span class="g-follow-wrapper">
+                                        <span class="g-follow" data-href="https://plus.google.com/+JasonBradley70" data-rel="author" data-height="15"></span>
+                                    </span>
+                                </div>
+                            </div>
+
                             <?php $publishing_options = get_option( 'theme_publishing_options' ); ?>
                             <?php $display_author_box = isset( $publishing_options['display_author_box'] ) ? $publishing_options['display_author_box'] : ''; ?>
                             <?php if( 'always' == $display_author_box ) { ?>
                                 <?php $social_options = get_option( 'theme_social_options' ); ?>
-                                <div id="author-box" class="media well">
+                                <div class="author-box media well">
                                     <div class="media-object pull-left">
                                         <?php echo get_avatar( get_the_author_meta( 'user_email', $post->post_author, '80' ) ); ?>
                                     </div><!-- /.author-box-image -->
                                     <div class="media-body">
                                         <h4 class="media-heading"><?php the_author_meta( 'display_name' ); ?></h4>
-                                        <p>
-                                            <a class="author-link author-posts-url" href="<?php echo trailingslashit( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" title="<?php echo get_the_author_meta( 'display_name' ); ?> <?php _e( 'Posts', TRANSLATION_KEY); ?>"><span class="fa fa-list-alt"></span> <?php _e( 'Posts', TRANSLATION_KEY ); ?></a>
-
-                                            <?php if( strlen( trim( get_the_author_meta( 'user_url' ) ) ) > 0 ) { ?>
-                                                <a class="author-link author-url" href="<?php echo trailingslashit( the_author_meta( 'user_url' ) ); ?>" title="<?php _e( 'Website', TRANSLATION_KEY); ?>" target="_blank" rel="author"><span class="fa fa-globe"></span> <?php _e( 'Website', TRANSLATION_KEY ); ?></a>
-                                            <?php } // end if ?>
-
-                                            <?php if( strlen( trim( get_user_meta( get_the_author_meta( 'ID' ), 'twitter', true ) ) ) > 0 ) { ?>
-                                                <a class="author-link icn-twitter" href="<?php echo trailingslashit( get_user_meta( get_the_author_meta( 'ID' ), 'twitter', true ) ); ?>" title="<?php _e( 'Twitter', TRANSLATION_KEY); ?>" target="_blank"><span class="fa fa-twitter"></span> <?php _e( 'Twitter', TRANSLATION_KEY); ?></a>
-                                            <?php } // end if ?>
-
-                                            <?php if( strlen( trim( get_user_meta( get_the_author_meta( 'ID' ), 'facebook', true ) ) ) > 0 ) { ?>
-                                                <a class="author-link icn-facebook" href="<?php echo trailingslashit( get_user_meta( get_the_author_meta( 'ID' ), 'facebook', true ) ); ?>" title="<?php _e( 'Facebook', TRANSLATION_KEY); ?>" target="_blank"><span class="fa fa-facebook"></span> <?php _e( 'Facebook', TRANSLATION_KEY); ?></a>
-                                            <?php } // end if ?>
-
-                                            <?php
-                                            // Get the Google+ ID based on if we're using the built in SEO or WordPress SEO
-                                            $google_plus =
-                                                using_native_seo() ?
-                                                    trailingslashit( get_user_meta( get_the_author_meta( 'ID' ), 'google_plus', true ) )
-                                                    :
-                                                    trailingslashit( get_user_meta( get_the_author_meta( 'ID' ), 'googleplus', true ) );
-                                            ?>
-
-                                            <?php if( 1 < strlen( trim( $google_plus ) ) ) { ?>
-                                                <a class="author-link icn-gplus" rel="author" href="<?php echo $google_plus; ?>" title="<?php _e( 'Google+', TRANSLATION_KEY); ?>" target="_blank"><span class="fa fa-google-plus"></span> <?php _e( 'Google+', TRANSLATION_KEY); ?></a>
-                                            <?php } // end if ?>
-                                        </p>
                                         <?php if( strlen( trim( the_author_meta( 'description' ) ) > 0 ) ) { ?>
-                                            <div class="author-box-description">
-                                                <p><?php the_author_meta( 'description' ); ?></p>
-                                            </div><!-- /.author-box-description -->
+                                            <p><?php the_author_meta( 'description' ); ?></p>
                                         <?php } // end if ?>
+                                        <p>
+                                            <ul class="list-inline">
+                                                <li>
+                                                    <a class="author-link author-posts-url" href="<?php echo trailingslashit( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" title="<?php echo get_the_author_meta( 'display_name' ); ?> <?php _e( 'Posts', TRANSLATION_KEY); ?>"><span class="fa fa-list-alt"></span> <?php _e( 'Posts', TRANSLATION_KEY ); ?></a>
+                                                </li>
+                                                <li>
+                                                    <?php if( strlen( trim( get_the_author_meta( 'user_url' ) ) ) > 0 ) { ?>
+                                                        <a class="author-link author-url" href="<?php echo trailingslashit( the_author_meta( 'user_url' ) ); ?>" title="<?php _e( 'Website', TRANSLATION_KEY); ?>" target="_blank" rel="author"><span class="fa fa-globe"></span> <?php _e( 'Website', TRANSLATION_KEY ); ?></a>
+                                                    <?php } // end if ?>
+                                                </li>
+                                                <li>
+                                                    <?php if( strlen( trim( get_user_meta( get_the_author_meta( 'ID' ), 'twitter', true ) ) ) > 0 ) { ?>
+                                                        <a class="author-link icn-twitter" href="<?php echo trailingslashit( get_user_meta( get_the_author_meta( 'ID' ), 'twitter', true ) ); ?>" title="<?php _e( 'Twitter', TRANSLATION_KEY); ?>" target="_blank"><span class="fa fa-twitter"></span> <?php _e( 'Twitter', TRANSLATION_KEY); ?></a>
+                                                    <?php } // end if ?>
+                                                </li>
+                                                <li>
+                                                    <?php if( strlen( trim( get_user_meta( get_the_author_meta( 'ID' ), 'facebook', true ) ) ) > 0 ) { ?>
+                                                        <a class="author-link icn-facebook" href="<?php echo trailingslashit( get_user_meta( get_the_author_meta( 'ID' ), 'facebook', true ) ); ?>" title="<?php _e( 'Facebook', TRANSLATION_KEY); ?>" target="_blank"><span class="fa fa-facebook"></span> <?php _e( 'Facebook', TRANSLATION_KEY); ?></a>
+                                                    <?php } // end if ?>
+                                                </li>
+
+                                                <?php
+                                                // Get the Google+ ID based on if we're using the built in SEO or WordPress SEO
+                                                $google_plus =
+                                                    using_native_seo() ?
+                                                        trailingslashit( get_user_meta( get_the_author_meta( 'ID' ), 'google_plus', true ) )
+                                                        :
+                                                        trailingslashit( get_user_meta( get_the_author_meta( 'ID' ), 'googleplus', true ) );
+                                                ?>
+                                                <li>
+                                                    <?php if( 1 < strlen( trim( $google_plus ) ) ) { ?>
+                                                        <a class="author-link icn-gplus" rel="author" href="<?php echo $google_plus; ?>" title="<?php _e( 'Google+', TRANSLATION_KEY); ?>" target="_blank"><span class="fa fa-google-plus"></span> <?php _e( 'Google+', TRANSLATION_KEY); ?></a>
+                                                    <?php } // end if ?>
+                                                </li>
+                                            </ul>
+                                        </p>
                                     </div>
                                 </div>
                             <?php } // end if ?>
