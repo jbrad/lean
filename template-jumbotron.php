80d653b67cf8c6062be587f08f2324b84794651f
jbrad <jason.bradley@me.com>
2014-03-18 20:28:32 -0500

    Removing unnecessary div row

--- template-jumbotron.php
+++ template-jumbotron.php
@@ -25,35 +25,33 @@
 
     <div class="wrapper features">
         <div class="container">
-            <div class="row">
-                <section class="col-md-12 clearfix" role="main">
-                    <?php if( ! is_offline() || is_user_logged_in() ) { ?>
-                        <div id="features" class="clearfix">
-                            <div class="container">
-                                <?php if( is_active_sidebar( 'sidebar-6' ) || is_active_sidebar( 'sidebar-7' ) || is_active_sidebar( 'sidebar-8' ) ) { ?>
-                                    <div class="row">
+            <section class="row" role="main">
+                <?php if( ! is_offline() || is_user_logged_in() ) { ?>
+                    <div id="features" class="clearfix">
+                        <div class="container">
+                            <?php if( is_active_sidebar( 'sidebar-6' ) || is_active_sidebar( 'sidebar-7' ) || is_active_sidebar( 'sidebar-8' ) ) { ?>
+                                <div class="row">
 
-                                        <div class="col-md-4 home-widgets clearfix">
-                                            <?php dynamic_sidebar( 'sidebar-6' ); ?>
-                                        </div><!-- /#left-footer-widget -->
+                                    <div class="col-md-4 home-widgets clearfix">
+                                        <?php dynamic_sidebar( 'sidebar-6' ); ?>
+                                    </div><!-- /#left-footer-widget -->
 
-                                        <div class="col-md-4 home-widgets clearfix">
-                                            <?php dynamic_sidebar( 'sidebar-7' ); ?>
-                                        </div><!-- /#center-footer-widget -->
+                                    <div class="col-md-4 home-widgets clearfix">
+                                        <?php dynamic_sidebar( 'sidebar-7' ); ?>
+                                    </div><!-- /#center-footer-widget -->
 
-                                        <div class="col-md-4 home-widgets clearfix">
-                                            <?php dynamic_sidebar( 'sidebar-8' ); ?>
-                                        </div><!-- /#right-footer-widget -->
+                                    <div class="col-md-4 home-widgets clearfix">
+                                        <?php dynamic_sidebar( 'sidebar-8' ); ?>
+                                    </div><!-- /#right-footer-widget -->
 
-                                    </div><!-- /row -->
-                                <?php } // end if ?>
-                                <hr class="featurette-divider">
-                                <?php featurettes(); ?>
-                            </div><!-- /container -->
-                        </div><!-- /#features -->
-                    <?php } // end if ?>
-                </section><!-- /#main -->
-            </div><!--/row -->
+                                </div><!-- /row -->
+                            <?php } // end if ?>
+                            <hr class="featurette-divider">
+                            <?php featurettes(); ?>
+                        </div><!-- /container -->
+                    </div><!-- /#features -->
+                <?php } // end if ?>
+            </section><!-- /#main -->
         </div><!-- /container -->
     </div> <!-- /#wrapper -->
 <?php get_footer(); ?>
