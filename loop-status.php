3164375c1ea179594dc30e6001e247a6960d5a90
jbrad <jason.bradley@me.com>
2014-03-20 11:54:48 -0500

    Resolves #44

--- loop-status.php
+++ loop-status.php
@@ -11,20 +11,18 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class( 'post format-status' ); ?>>
 
-    <div class="post-header clearfix">
-        <div class="row">
-            <div class="post-avatar col-md-2 col-sm-4 col-xs-12">
+    <div class="post-header media well">
+        <div class="media-object pull-left">
                 <?php echo get_avatar( get_the_author_meta( 'ID' ), 80 ); ?>
-            </div><!-- /.post-avatar -->
-            <div class="entry-content col-md-10 col-sm-8 col-xs-12 clearfix">
-                <?php if( ( is_category() || is_archive() || is_home() ) && has_excerpt() ) { ?>
-                    <?php the_excerpt( ); ?>
-                    <a href="<?php echo get_permalink(); ?>"><?php _e( 'Continue Reading...', TRANSLATION_KEY ); ?></a>
-                <?php } else { ?>
-                    <?php the_content( __( 'Continue Reading...', TRANSLATION_KEY ) ); ?>
-                <?php } // end if/else ?>
-            </div><!-- /.entry-content -->
-        </div><!-- /row -->
+        </div><!-- /.post-avatar -->
+        <div class="entry-content media-body">
+            <?php if( ( is_category() || is_archive() || is_home() ) && has_excerpt() ) { ?>
+                <?php the_excerpt( ); ?>
+                <a href="<?php echo get_permalink(); ?>"><?php _e( 'Continue Reading...', TRANSLATION_KEY ); ?></a>
+            <?php } else { ?>
+                <?php the_content( __( 'Continue Reading...', TRANSLATION_KEY ) ); ?>
+            <?php } // end if/else ?>
+        </div><!-- /.entry-content -->
     </div> <!-- /.post-header -->
 
     <div class="post-meta text-muted clearfix">
